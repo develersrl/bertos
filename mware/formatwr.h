c273ae81bac91c7eaaecb71482992803539dd0eb
bernie <bernie@38d2e660-2303-0410-9eaa-f027e97ec537>
2004-07-29 22:57:09 +0000

    Add values for new-style CONFIG_PRINTF option.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@86 38d2e660-2303-0410-9eaa-f027e97ec537

--- mware/formatwr.h
+++ mware/formatwr.h
@@ -1,7 +1,7 @@
 /*!
  * \file
  * <!--
- * Copyright 2003,2004 Develer S.r.l. (http://www.develer.com/)
+ * Copyright 2003, 2004 Develer S.r.l. (http://www.develer.com/)
  * This file is part of DevLib - See devlib/README for information.
  * -->
  *
@@ -12,6 +12,9 @@
 
 /*
  * $Log$
+ * Revision 1.3  2004/07/29 22:57:09  bernie
+ * Add values for new-style CONFIG_PRINTF option.
+ *
  * Revision 1.2  2004/06/03 11:27:09  bernie
  * Add dual-license information.
  *
@@ -24,18 +27,30 @@
 
 #include <stdarg.h>
 
+/*!
+ * \name _formatted_write() configuration
+ * \{
+ */
+#define PRINTF_DISABLED    0
+#define PRINTF_NOMODIFIERS 1
+#define PRINTF_REDUCED     2
+#define PRINTF_NOFLOAT     3
+#define PRINTF_FULL        4
+/* \} */
+
+
 int _formatted_write(
 	const char *format,
-	void put_one_char(char, void *),
-	void *secret_pointer,
+	void put_char_func(char c, void *user_data),
+	void *user_data,
 	va_list ap);
 
 #ifdef __AVR__
 	#include <avr/pgmspace.h>
 	int _formatted_write_P(
 		const char * PROGMEM format,
-		void put_one_char(char, void *),
-		void *secret_pointer,
+		void put_char_func(char c, void *user_data),
+		void *user_data,
 		va_list ap);
 #endif /* __AVR__ */
 
