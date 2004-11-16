dde98914ec05107d91e12797f93d4a55dfb5b95c
bernie <bernie@38d2e660-2303-0410-9eaa-f027e97ec537>
2004-11-16 21:04:23 +0000

    Update to new naming scheme in mware/gfx.c.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@274 38d2e660-2303-0410-9eaa-f027e97ec537

--- mware/charts.c
+++ mware/charts.c
@@ -26,6 +26,9 @@
 
 /*#*
  *#* $Log$
+ *#* Revision 1.6  2004/11/16 21:04:23  bernie
+ *#* Update to new naming scheme in mware/gfx.c.
+ *#*
  *#* Revision 1.5  2004/09/14 20:56:39  bernie
  *#* Make more generic and adapt to new gfx functions.
  *#*
@@ -48,9 +51,10 @@
 
 void chart_init(Bitmap *bm, coord_t xmin, coord_t ymin, coord_t xmax, coord_t ymax)
 {
-	gfx_ClearBitmap(bm);
+	/* Clear the chart area */
+	gfx_rectClear(bm, xmin, ymin, xmax, ymax);
 
-	gfx_SetClipRect(bm, xmin + CHART_BORDERLEFT, ymin + CHART_BORDERTOP,
+	gfx_setClipRect(bm, xmin + CHART_BORDERLEFT, ymin + CHART_BORDERTOP,
 		xmax - CHART_BORDERRIGHT, ymax - CHART_BORDERBOTTOM);
 
 	chart_drawAxis(bm);
@@ -59,7 +63,7 @@ void chart_init(Bitmap *bm, coord_t xmin, coord_t ymin, coord_t xmax, coord_t ym
 
 void chart_setScale(Bitmap *bm, chart_x_t xmin, chart_y_t ymin, chart_x_t xmax, chart_y_t ymax)
 {
-	gfx_SetViewRect(bm, xmin, ymin, xmax, ymax);
+	gfx_setViewRect(bm, xmin, ymin, xmax, ymax);
 }
 
 
@@ -71,26 +75,26 @@ void chart_drawAxis(Bitmap *bm)
 #if CONFIG_CHART_ARROWS
 
 	/* Draw axis */
-	gfx_MoveTo(bm, bm->cr.xmin, bm->cr.ymin + 4);
-	gfx_LineTo(bm, bm->cr.xmin, bm->cr.ymax - 1);
-	gfx_LineTo(bm, bm->cr.xmax - 4 - 1, bm->cr.ymax - 1);
+	gfx_moveTo(bm, bm->cr.xmin, bm->cr.ymin + 4);
+	gfx_lineTo(bm, bm->cr.xmin, bm->cr.ymax - 1);
+	gfx_lineTo(bm, bm->cr.xmax - 4 - 1, bm->cr.ymax - 1);
 
 	/* Draw up arrow */
-	gfx_MoveTo(bm, bm->cr.xmin - 2, bm->cr.ymin + 3);
-	gfx_LineTo(bm, bm->cr.xmin + 2, bm->cr.ymin + 3);
-	gfx_LineTo(bm, bm->cr.xmin, bm->cr.ymin);
-	gfx_LineTo(bm, bm->cr.xmin - 2, bm->cr.ymin + 3);
+	gfx_moveTo(bm, bm->cr.xmin - 2, bm->cr.ymin + 3);
+	gfx_lineTo(bm, bm->cr.xmin + 2, bm->cr.ymin + 3);
+	gfx_lineTo(bm, bm->cr.xmin, bm->cr.ymin);
+	gfx_lineTo(bm, bm->cr.xmin - 2, bm->cr.ymin + 3);
 
 	/* Draw right arrow */
-	gfx_MoveTo(bm, bm->cr.xmax - 4, bm->cr.ymax - 3);
-	gfx_LineTo(bm, bm->cr.xmax - 4, bm->cr.ymax + 1);
-	gfx_LineTo(bm, bm->cr.xmax - 1, bm->cr.ymax - 1);
-	gfx_LineTo(bm, bm->cr.xmax - 4, bm->cr.ymax - 3);
+	gfx_moveTo(bm, bm->cr.xmax - 4, bm->cr.ymax - 3);
+	gfx_lineTo(bm, bm->cr.xmax - 4, bm->cr.ymax + 1);
+	gfx_lineTo(bm, bm->cr.xmax - 1, bm->cr.ymax - 1);
+	gfx_lineTo(bm, bm->cr.xmax - 4, bm->cr.ymax - 3);
 
 #else /* CONFIG_CHART_ARROWS */
 
 	/* Draw a box around the chart */
-	gfx_RectDraw(bm, bm->cr.xmin, bm->cr.ymin, bm->cr.xmax, bm->cr.ymax);
+	gfx_rectDraw(bm, bm->cr.xmin, bm->cr.ymin, bm->cr.xmax, bm->cr.ymax);
 
 #endif /* CONFIG_CHART_ARROWS */
 
@@ -108,10 +112,10 @@ void chart_drawCurve(Bitmap *bm, const chart_y_t *curve_y, int curve_cnt)
 {
 	int i;
 
-	gfx_MoveTo(bm, gfx_TransformX(bm, 0), gfx_TransformY(bm, curve_y[0]));
+	gfx_moveTo(bm, gfx_transformX(bm, 0), gfx_transformY(bm, curve_y[0]));
 
 	for (i = 1; i < curve_cnt; i++)
-		gfx_LineTo(bm, gfx_TransformX(bm, i), gfx_TransformY(bm, curve_y[i]));
+		gfx_lineTo(bm, gfx_transformX(bm, i), gfx_transformY(bm, curve_y[i]));
 
 	//CHECK_WALL(wall_before_raster, WALL_SIZE);
 	//CHECK_WALL(wall_after_raster, WALL_SIZE);
@@ -130,19 +134,19 @@ void chart_drawDots(Bitmap *bm, const chart_x_t *dots_x, const chart_y_t *dots_y
 	for (i = 0; i < cnt; i++)
 	{
 		if (dots_x)
-			x = gfx_TransformX(bm, dots_x[i]);
+			x = gfx_transformX(bm, dots_x[i]);
 		else
-			x = gfx_TransformX(bm, i);
+			x = gfx_transformX(bm, i);
 
-		y = gfx_TransformY(bm, dots_y[i]);
+		y = gfx_transformY(bm, dots_y[i]);
 
 		/* Draw tick over the curve */
-		gfx_RectFill(bm,
+		gfx_rectFill(bm,
 			x - TICKS_WIDTH / 2, y - TICKS_HEIGHT / 2,
 			x + (TICKS_WIDTH + 1) / 2, y + (TICKS_HEIGHT + 1) / 2);
 
 		/* Draw vertical line from the curve to the X-axis */
-		//gfx_DrawLine(bm, x, y, x, bm->cr.ymax - 1);
+		//gfx_drawLine(bm, x, y, x, bm->cr.ymax - 1);
 	}
 
 	//CHECK_WALL(wall_before_raster, WALL_SIZE);
