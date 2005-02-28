e610d72063523ad00d20292275ed1e76569b6cfe
bernie <bernie@38d2e660-2303-0410-9eaa-f027e97ec537>
2005-02-28 10:46:44 +0000

    Remove test binaries.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@380 38d2e660-2303-0410-9eaa-f027e97ec537

--- run_tests.sh
+++ run_tests.sh
@@ -8,6 +8,9 @@
 # $Id$
 #
 # $Log$
+# Revision 1.2  2005/02/28 10:46:44  bernie
+# Remove test binaries.
+#
 # Revision 1.1  2005/02/01 06:59:24  bernie
 # Really trivial testsuite framework.
 #
@@ -28,10 +31,12 @@ for test in `find . -name "*_test.*"`; do
 	*.cpp)
 		$CXX $CXXFLAGS $test -o test || exit 1
 		./test || exit 1
+		rm -f test
 	;;
 	*.c)
 		$CC $CFLAGS $test -o test || exit 1
 		./test || exit 1
+		rm -f test
 	;;
 	esac
 done
