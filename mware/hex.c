8ea8887c13de8a1f39412335ab96a4822f192c8a
bernie <bernie@38d2e660-2303-0410-9eaa-f027e97ec537>
2005-03-01 23:23:25 +0000

    Poor man's factored hex table.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@382 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ mware/hex.c
@@ -0,0 +1,12 @@
+/*!
+ * \file
+ * <!--
+ * Copyright 2005 Develer S.r.l. (http://www.develer.com/)
+ * This file is part of DevLib - See README.devlib for information.
+ * -->
+ *
+ * \brief Poor man's hex arrays (implementation).
+ */
+
+const char hex_tab[16] = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
+const char HEX_tab[16] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };
---
+++ mware/hex.h
@@ -0,0 +1,17 @@
+/*!
+ * \file
+ * <!--
+ * Copyright 2005 Develer S.r.l. (http://www.develer.com/)
+ * This file is part of DevLib - See README.devlib for information.
+ * -->
+ *
+ * \brief Poor man's hex arrays (implementation).
+ */
+
+#ifndef MWARE_HEX_H
+#define MWARE_HEX_H
+
+extern const char hex_tab[16];
+extern const char HEX_tab[16];
+
+#endif /* MWARE_HEX_H */
