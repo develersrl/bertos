c49c99e64ee64ad490b2aea2d6c6d8f9f6d225db
bernie <bernie@38d2e660-2303-0410-9eaa-f027e97ec537>
2006-03-22 09:52:13 +0000

    Add demo application.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@587 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ app/demo/demo.c
@@ -0,0 +1,226 @@
+/**
+ * \file
+ * <!--
+ * Copyright 2006 Develer S.r.l. (http://www.develer.com/)
+ * This file is part of DevLib - See README.devlib for information.
+ * -->
+ *
+ * \version $Id$
+ *
+ * \author Bernardo Innocenti <bernie@develer.com>
+ *
+ * \brief Windowing system test.
+ */
+
+/*#*
+ *#* $Log$
+ *#* Revision 1.1  2006/03/22 09:52:13  bernie
+ *#* Add demo application.
+ *#*
+ *#* Revision 1.1  2006/01/23 23:14:29  bernie
+ *#* Implement simple, but impressive windowing system.
+ *#*
+ *#*/
+
+#include <emul/emul.h>
+#include <kern/proc.h>
+#include <drv/timer.h>
+#include <drv/buzzer.h>
+#include <drv/lcd_gfx.h>
+#include <drv/kbd.h>
+#include <gfx/gfx.h>
+#include <gfx/win.h>
+#include <gfx/text.h>
+#include <gfx/font.h>
+#include <icons/artwork.h>
+#include <mware/menu.h>
+#include <cfg/macros.h>
+
+
+/* SETTINGS SUBMENU ***************************/
+
+static struct MenuItem settings_items[] =
+{
+	{ (const_iptr_t)"settings_0", 0, (MenuHook)0,  (iptr_t)0        },
+	{ (const_iptr_t)"settings_1", 0, (MenuHook)0,  (iptr_t)0        },
+	{ (const_iptr_t)"settings_2", 0, (MenuHook)0, (iptr_t)0      },
+	{ (const_iptr_t)"settings_3", 0, (MenuHook)0,  (iptr_t)0   },
+	{ (const_iptr_t)"settings_4", 0, (MenuHook)0,  (iptr_t)0 },
+	{ (const_iptr_t)"settings_5", MIF_TOGGLE, (MenuHook)0, (iptr_t)0             },
+	{ (const_iptr_t)0, 0, NULL, (iptr_t)0 }
+};
+static struct Menu settings_menu = { settings_items, "Settings Menu", MF_STICKY, &lcd_bitmap, 0 };
+
+/*** DISPLAY MENU ****************************/
+
+static struct MenuItem display_items[] =
+{
+	{ (const_iptr_t)"display_0", 0, (MenuHook)0,           (iptr_t)0 },
+	{ (const_iptr_t)"display_1", 0, (MenuHook)0, (iptr_t)0     },
+	{ (const_iptr_t)"display_2", 0, (MenuHook)0,   (iptr_t)0     },
+	{ (const_iptr_t)"display_3", 0, (MenuHook)0,  (iptr_t)0 },
+	{ (const_iptr_t)0, 0, NULL, (iptr_t)0 }
+};
+static struct Menu display_menu = { display_items, "Display Menu", MF_STICKY, &lcd_bitmap, 0 };
+
+
+/*** SETUP MENU ******************************/
+
+static struct MenuItem setup_items[] =
+{
+	{ (const_iptr_t)"Setup 0", 0, (MenuHook)NULL, (iptr_t)0    },
+	{ (const_iptr_t)"Setup 1", 0, (MenuHook)NULL, (iptr_t)0    },
+	{ (const_iptr_t)"Setup 2", 0, (MenuHook)NULL, (iptr_t)0    },
+	{ (const_iptr_t)"Setup 3", 0, (MenuHook)NULL, (iptr_t)0    },
+	{ (const_iptr_t)0, 0, NULL, NULL }
+};
+static struct Menu setup_menu = { setup_items, "Setup Menu", MF_STICKY, &lcd_bitmap, 0 };
+
+
+/*** MAIN MENU *******************************/
+
+static struct MenuItem main_items[] =
+{
+	{ (const_iptr_t)"Settings", 0, (MenuHook)menu_handle, (iptr_t)&settings_menu },
+	{ (const_iptr_t)"Display", 0, (MenuHook)menu_handle, (iptr_t)&display_menu  },
+	{ (const_iptr_t)"Setup",   0, (MenuHook)menu_handle, (iptr_t)&setup_menu    },
+	{ (const_iptr_t)0, 0, NULL, (iptr_t)0 }
+};
+static struct Menu main_menu = { main_items, "Main Menu", MF_STICKY, &lcd_bitmap, 0 };
+
+static void magic(struct Bitmap *bitmap, coord_t x, coord_t y)
+{
+	static const coord_t coords[] = { 120, 34, 90, 90, 30, 90, 0, 34, 60, 0, 90, 90, 0, 34, 120, 34, 30, 90, 60, 0 };
+	unsigned int i;
+
+	gfx_moveTo(bitmap, coords[countof(coords)-2]/2 + x, coords[countof(coords)-1]/3 + y);
+	for (i = 0; i < countof(coords); i += 2)
+		gfx_lineTo(bitmap, coords[i]/2 + x, coords[i+1]/3 + y);
+}
+
+Window root_win;
+
+void schedule(void)
+{
+//	win_compose(&root_win);
+	lcd_blit_bitmap(root_win.bitmap);
+	emul_idle();
+	usleep(10000);
+}
+
+int main(int argc, char *argv[])
+{
+	emul_init(&argc, argv);
+	timer_init();
+	buz_init();
+	kbd_init();
+	lcd_init();
+	proc_init();
+
+	gfx_bitmapClear(&lcd_bitmap);
+	extern const Font font_10x20;
+	extern const Font font_helvB10;
+	extern const Font font_courB08;
+	extern const Font font_courB14;
+	extern const Font font_mono17;
+	extern const Font font_luBS14;
+	extern const Font font_ncenB18;
+	gfx_setFont(&lcd_bitmap, &font_luBS14);
+
+	text_xprintf(&lcd_bitmap, 1, 0, STYLEF_BOLD | TEXT_FILL | TEXT_CENTER, "Hello, world!");
+	for (int i = 0; i < 1000; ++i)
+	{
+		lcd_blit_bitmap(&lcd_bitmap);
+		emul_idle();
+	}
+	gfx_blitRaster(&lcd_bitmap, 0, 0, customer_pw_logo, 122, 32, 4);
+	lcd_blit_bitmap(&lcd_bitmap);
+	emul_idle();
+	sleep(1);
+	//timer_delay(1000);
+
+	const coord_t small_left = 45, small_top = 30, small_width = 50, small_height = 30;
+	const coord_t large_left = -10, large_top = 10, large_width = 85, large_height = 41;
+
+	Window small_win, large_win;
+	Bitmap small_bm, large_bm;
+	uint8_t small_raster[RASTER_SIZE(small_width, small_height)];
+	uint8_t large_raster[RASTER_SIZE(large_width, large_height)];
+
+	win_create(&root_win,  &lcd_bitmap);
+
+	gfx_bitmapInit(&large_bm, large_raster, large_width, large_height);
+	win_create(&large_win, &large_bm);
+	win_open(&large_win, &root_win);
+	win_move(&large_win, large_left, large_top);
+
+	gfx_bitmapInit(&small_bm, small_raster, small_width, small_height);
+	win_create(&small_win, &small_bm);
+	win_open(&small_win, &root_win);
+	win_move(&small_win, small_left, small_top);
+
+
+	coord_t x = 0, y = LCD_WIDTH / 2;
+	coord_t xdir = +1, ydir = -1;
+	coord_t xdir_large = +1;
+	coord_t ydir_small = +1;
+	int raise_counter = 0;
+	int i;
+	Bitmap *bm;
+
+	for(;;)
+	{
+		/* Background animation */
+		bm = &lcd_bitmap;
+		gfx_bitmapClear(bm);
+/*		gfx_setClipRect(bm, 0, 0, bm->width, bm->height);
+		gfx_rectDraw(bm, 10, 10, bm->width-10, bm->height-10);
+		gfx_setClipRect(bm, 11, 11, bm->width-11, bm->height-11);
+*/		magic(bm, x, y);
+		x += xdir;
+		y += ydir;
+		if (x >= bm->width)  xdir = -1;
+		if (x <= -50)        xdir = +1;
+		if (y >= bm->height) ydir = -1;
+		if (y <= -50)        ydir = +1;
+
+		menu_handle(&main_menu);
+
+		/* Large window animation */
+		bm = large_win.bitmap;
+		gfx_bitmapClear(bm);
+		for (i = 0; i < bm->height / 2; i += 2)
+			gfx_rectDraw(bm, 0 + i, 0 + i, bm->width - i, bm->height - i);
+
+
+		/* Small window animation */
+		bm = small_win.bitmap;
+		gfx_bitmapClear(bm);
+		gfx_rectDraw(bm, 0, 0, bm->width, bm->height);
+		gfx_line(bm, 0, 0, bm->width, bm->height);
+		gfx_line(bm, bm->width, 0, 0, bm->height);
+
+		/* Move windows around */
+		win_move(&large_win, large_win.geom.xmin + xdir_large, large_top);
+		if (large_win.geom.xmin < -20) xdir_large = +1;
+		if (large_win.geom.xmin > RECT_WIDTH(&root_win.geom) - 5) xdir_large = -1;
+
+		win_move(&small_win, small_left, small_win.geom.ymin + ydir_small);
+		if (small_win.geom.ymin < -20) ydir_small = +1;
+		if (small_win.geom.ymin > RECT_HEIGHT(&root_win.geom) - 5) ydir_small = -1;
+
+		++raise_counter;
+		if (raise_counter % 997 == 0)
+			win_raise(&small_win);
+		else if (raise_counter % 731 == 0)
+			win_raise(&large_win);
+
+		win_compose(&root_win);
+		lcd_blit_bitmap(root_win.bitmap);
+		emul_idle();
+		usleep(10000);
+	}
+
+	emul_cleanup();
+	return 0;
+}
---
+++ app/demo/demo.mk
@@ -0,0 +1,78 @@
+#
+# $Id$
+# Copyright 2003, 2004, 2005, 2006 Develer S.r.l. (http://www.develer.com/)
+# All rights reserved.
+#
+# Makefile fragment for DevLib demo application.
+#
+# Author: Bernardo Innocenti <bernie@develer.com>
+#
+# $Log$
+# Revision 1.1  2006/03/22 09:52:13  bernie
+# Add demo application.
+#
+#
+
+
+# Set to 1 for debug builds
+demo_DEBUG = 1
+
+# Our target application
+TRG += demo
+
+CC = g++
+CXX = g++
+
+demo_CXXSRC = \
+	emul/emul.cpp \
+	emul/emulwin.cpp \
+	emul/emulkbd.cpp \
+	drv/lcd_gfx_qt.cpp
+
+demo_CSRC = \
+	app/demo/demo.c \
+	os/hptime.c \
+	gfx/bitmap.c \
+	gfx/line.c \
+	gfx/win.c \
+	gfx/text.c \
+	gfx/text_format.c \
+	fonts/helvB10.c \
+	fonts/luBS14.c \
+	fonts/ncenB18.c \
+	icons/artwork.c \
+	drv/kbd.c \
+	drv/timer.c \
+	drv/buzzer.c \
+	drv/ser.c \
+	drv/ser_posix.c \
+	mware/formatwr.c \
+	mware/hex.c \
+	mware/event.c \
+	mware/menu.c \
+	mware/observer.c \
+	mware/resource.c \
+	mware/sprintf.c \
+	kern/proc.c \
+	kern/sem.c \
+	kern/signal.c \
+	kern/monitor.c
+
+demo_ASRC = \
+	kern/switch_x86_64.s
+
+
+$(OBJDIR)/demo/emul/emulwin.o: emul/emulwin_moc.cpp 
+$(OBJDIR)/demo/drv/lcd_gfx_qt.o: drv/lcd_gfx_qt_moc.cpp
+
+EMUL_CFLAGS = -I/usr/local/kde4/include/Qt -I/usr/local/kde4/include -DQT_CLEAN_NAMESPACE -DQT3_SUPPORT
+EMUL_LDFLAGS = -L /usr/local/kde4/lib64 -lQtGui -lQtCore
+demo_CFLAGS = -Os -D_QT=4 -D'ARCH=ARCH_EMUL' -Ihw $(EMUL_CFLAGS)
+demo_LDFLAGS = $(EMUL_LDFLAGS)
+
+# Debug stuff
+#ifeq ($(demo_DEBUG),1)
+#	demo_CFLAGS += -D_DEBUG
+#	demo_PCSRC += drv/kdebug.c
+#endif
+
