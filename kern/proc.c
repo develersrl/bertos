a9b5312678be2d66d3ecaa5e8c33f01c7a2dbf15
marco <marco@38d2e660-2303-0410-9eaa-f027e97ec537>
2006-09-20 14:19:23 +0000

    Restored test.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@709 38d2e660-2303-0410-9eaa-f027e97ec537

--- kern/proc.c
+++ kern/proc.c
@@ -17,6 +17,9 @@
 
 /*#*
  *#* $Log$
+ *#* Revision 1.32  2006/09/20 14:19:23  marco
+ *#* Restored test.
+ *#*
  *#* Revision 1.31  2006/07/19 12:56:27  bernie
  *#* Convert to new Doxygen style.
  *#*
@@ -477,62 +480,3 @@ void proc_permit(void)
 }
 
 #endif /* CONFIG_KERN_PREEMPTIVE */
-
-
-#if 0 /* Simple testcase for the scheduler */
-
-#include <drv/timer.h>
-
-/**
- * Proc scheduling test subthread 1
- */
-static void NORETURN proc_test_thread1(void)
-{
-	for (;;)
-	{
-		kputs(">task 1\n");
-		timer_delay(50);
-		proc_switch();
-	}
-}
-
-/**
- * Proc scheduling test subthread 2
- */
-static void NORETURN proc_test_thread2(void)
-{
-	for (;;)
-	{
-		kputs(">task 2\n");
-		timer_delay(75);
-		proc_switch();
-	}
-}
-
-static cpustack_t proc_test_stack1[CONFIG_KERN_DEFSTACKSIZE/sizeof(cpustack_t)];
-static cpustack_t proc_test_stack2[CONFIG_KERN_DEFSTACKSIZE/sizeof(cpustack_t)];
-
-/**
- * Proc scheduling test
- */
-void NORETURN proc_test(void)
-{
-	proc_new(proc_test_thread1, NULL, sizeof(proc_test_stack1), proc_test_stack1);
-	proc_new(proc_test_thread2, NULL, sizeof(proc_test_stack2), proc_test_stack2);
-	kputs("Created tasks\n");
-
-	kputs("stack1:\n");
-	kdump(proc_test_stack1+sizeof(proc_test_stack1)-64, 64);
-	kputs("stack2:\n");
-	kdump(proc_test_stack2+sizeof(proc_test_stack1)-64, 64);
-
-	for (;;)
-	{
-		kputs(">main task\n");
-		timer_delay(93);
-		proc_switch();
-	}
-
-	ASSERT(false);
-}
-#endif
