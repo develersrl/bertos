064207cb164caa49ec289c3a83f8ecf1a9b71039
marco <marco@38d2e660-2303-0410-9eaa-f027e97ec537>
2006-09-20 14:51:41 +0000

    Fonts make.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@716 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ fonts/fonts.mk
@@ -0,0 +1,12 @@
+#
+# $Id$
+# Copyright 2003, 2004, 2005, 2006 Develer S.r.l. (http://www.develer.com/)
+# All rights reserved.
+#
+# Makefile fragment for DevLib demo application.
+#
+# Author: Bernardo Innocenti <bernie@develer.com>
+#
+
+%.c: %.bdf
+	fonts/convbdf $< > $@
