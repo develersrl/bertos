3d95d544d3edaffd6e547d26bc56deacfa0e94f7
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2007-10-30 15:49:33 +0000

    Remove cvs logs.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@969 38d2e660-2303-0410-9eaa-f027e97ec537

--- mware/xmodem.h
+++ mware/xmodem.h
@@ -37,33 +37,7 @@
  * \author Francesco Sacchi <batt@develer.com>
  */
 
-/*#*
- *#* $Log$
- *#* Revision 1.7  2007/06/14 14:42:28  batt
- *#* Move kfile to kern/ directory; remove duplicate file.h.
- *#*
- *#* Revision 1.6  2007/06/07 09:10:44  batt
- *#* Fix some todos.
- *#*
- *#* Revision 1.5  2006/07/19 12:56:28  bernie
- *#* Convert to new Doxygen style.
- *#*
- *#* Revision 1.4  2005/11/04 16:20:02  bernie
- *#* Fix reference to README.devlib in header.
- *#*
- *#* Revision 1.3  2004/08/25 14:12:09  rasky
- *#* Aggiornato il comment block dei log RCS
- *#*
- *#* Revision 1.2  2004/08/15 05:31:46  bernie
- *#* Add an #error to spread some FUD about the quality of this module;
- *#* Add a few TODOs from Rasky's review;
- *#* Update to the new drv/ser.c API;
- *#* Move FlushSerial() to drv/ser.c and generalize.
- *#*
- *#* Revision 1.1  2004/08/11 19:54:22  bernie
- *#* Import XModem protocol into DevLib.
- *#*
- *#*/
+
 #ifndef MWARE_XMODEM_H
 #define MWARE_XMODEM_H
 #include <cfg/compiler.h>
