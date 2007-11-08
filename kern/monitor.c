9f05a8899a99750b8d7b550b0323ee17e687a4f6
batt <batt@38d2e660-2303-0410-9eaa-f027e97ec537>
2007-11-08 18:19:32 +0000

    Fix some warrings. Remove cvs logs.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@994 38d2e660-2303-0410-9eaa-f027e97ec537

--- kern/monitor.c
+++ kern/monitor.c
@@ -37,45 +37,6 @@
  * \author Giovanni Bajo <rasky@develer.com>
  */
 
-/*#*
- *#* $Log$
- *#* Revision 1.10  2006/09/20 13:58:42  marco
- *#* Used z modifier instead l.
- *#*
- *#* Revision 1.9  2006/07/19 12:56:27  bernie
- *#* Convert to new Doxygen style.
- *#*
- *#* Revision 1.8  2006/03/22 09:49:22  bernie
- *#* Reduce memory usage.
- *#*
- *#* Revision 1.7  2006/03/13 02:07:14  bernie
- *#* Add TODO item.
- *#*
- *#* Revision 1.6  2006/02/24 01:17:05  bernie
- *#* Update for new emulator.
- *#*
- *#* Revision 1.5  2005/11/04 16:20:02  bernie
- *#* Fix reference to README.devlib in header.
- *#*
- *#* Revision 1.4  2005/04/11 19:10:28  bernie
- *#* Include top-level headers from cfg/ subdir.
- *#*
- *#* Revision 1.3  2004/11/28 23:20:25  bernie
- *#* Remove obsolete INITLIST macro.
- *#*
- *#* Revision 1.2  2004/10/26 09:01:15  bernie
- *#* monitor_rename(): New function.
- *#*
- *#* Revision 1.1  2004/10/03 20:39:03  bernie
- *#* Import in DevLib.
- *#*
- *#* Revision 1.2  2004/10/03 20:36:43  bernie
- *#* Use debug.h instead of drv/kdebug.h; Misc spacing/header fixes.
- *#*
- *#* Revision 1.1  2004/09/30 23:19:30  rasky
- *#* Estratto il monitor degli stack da proc.c in due file a parte: monitor.c/h
- *#* Rinominata monitor_debug_stacks in monitor_report
- *#*/
 
 #include "monitor.h"
 
@@ -120,7 +81,7 @@ void monitor_rename(Process *proc, const char* name)
 
 /* TODO: use containerof() */
 #define MONITOR_NODE_TO_PROCESS(node) \
-	(struct Process *)((char *)(node) - offsetof(struct Process, monitor.link))
+	(struct Process *)((intptr_t)(node) - offsetof(struct Process, monitor.link))
 
 size_t monitor_checkStack(cpustack_t* stack_base, size_t stack_size)
 {
