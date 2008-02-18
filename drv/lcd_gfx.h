cec5b70116a26892b1482b7d6ea22e30f4d0a713
bernie <bernie@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-02-18 14:56:27 +0000

    Add file that was never committed to CVS for a long time.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1117 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ drv/lcd_gfx.h
@@ -0,0 +1,37 @@
+/**
+ * \file
+ * <!--
+ * Copyright 2003, 2004, 2006 Develer S.r.l. (http://www.develer.com/)
+ * Copyright 2001 Bernardo Innocenti <bernie@codewiz.org>
+ * This file is part of DevLib - See README.devlib for information.
+ * -->
+ *
+ * \version $Id: lcd_gfx.h,v 1.6 2006/07/19 12:56:25 bernie Exp $
+ *
+ * \author Bernardo Innocenti <bernie@develer.com>
+ * \author Stefano Fedrigo <aleph@develer.com>
+ *
+ * \brief Displaytech 32122A LCD driver
+ */
+#ifndef LCD_GFX_H
+#define LCD_GFX_H
+
+/* Predefined LCD PWM contrast values */
+#define LCD_DEF_PWM 145
+#define LCD_MAX_PWM 505
+#define LCD_MIN_PWM 130
+
+/* Display bitmap dims */
+#define LCD_WIDTH  128
+#define LCD_HEIGHT 64
+
+/* fwd decl */
+struct Bitmap;
+
+extern struct Bitmap lcd_bitmap;
+
+void lcd_init(void);
+void lcd_setPwm(int duty);
+void lcd_blitBitmap(struct Bitmap *bm);
+
+#endif /* LCD_GFX_H */
