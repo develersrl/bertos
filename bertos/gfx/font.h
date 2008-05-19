8b3bd9c1b2c7b8ce1f3fc87c37a03030dca914ee
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-05-19 07:48:47 +0000

    Remove CVS logs.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1270 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/gfx/font.h
+++ bertos/gfx/font.h
@@ -37,54 +37,6 @@
  * \brief Font 8x6 IBM-PC 8bit
  */
 
-/*#*
- *#* $Log$
- *#* Revision 1.7  2006/07/19 12:56:26  bernie
- *#* Convert to new Doxygen style.
- *#*
- *#* Revision 1.6  2006/05/27 22:31:34  bernie
- *#* Switch to a smaller default fount.
- *#*
- *#* Revision 1.5  2006/03/13 02:06:04  bernie
- *#* FONT_HAS_GLYPH(): New macro.
- *#*
- *#* Revision 1.4  2006/02/15 09:10:15  bernie
- *#* Implement prop fonts; Fix algo styles.
- *#*
- *#* Revision 1.3  2006/02/10 12:29:05  bernie
- *#* Add multiple font support in bitmaps.
- *#*
- *#* Revision 1.2  2005/11/04 18:17:45  bernie
- *#* Fix header guards and includes for new location of gfx module.
- *#*
- *#* Revision 1.1  2005/11/04 18:11:35  bernie
- *#* Move graphics stuff from mware/ to gfx/.
- *#*
- *#* Revision 1.6  2005/04/11 19:10:28  bernie
- *#* Include top-level headers from cfg/ subdir.
- *#*
- *#* Revision 1.5  2005/03/01 23:26:45  bernie
- *#* Use new CPU-neutral program-memory API.
- *#*
- *#* Revision 1.4  2004/12/31 16:42:55  bernie
- *#* Sanitize for non-Harvard processors.
- *#*
- *#* Revision 1.3  2004/08/25 14:12:09  rasky
- *#* Aggiornato il comment block dei log RCS
- *#*
- *#* Revision 1.2  2004/06/03 11:27:09  bernie
- *#* Add dual-license information.
- *#*
- *#* Revision 1.1  2004/05/23 15:43:16  bernie
- *#* Import mware modules.
- *#*
- *#* Revision 1.2  2004/03/24 15:48:53  bernie
- *#* Remove Copyright messages from Doxygen output
- *#*
- *#* Revision 1.1  2004/01/13 12:15:28  aleph
- *#* Move font table in program memory; add font.h
- *#*
- *#*/
 #ifndef GFX_FONT_H
 #define GFX_FONT_H
 
