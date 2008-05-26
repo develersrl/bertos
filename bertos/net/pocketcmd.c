9ad4bfda67375c05ca590cc793da190b9dc8de5b
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-05-26 15:21:44 +0000

    Reformat. Add header.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1373 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/net/pocketcmd.c
+++ bertos/net/pocketcmd.c
@@ -1,12 +1,33 @@
 /**
  * \file
  * <!--
+ * This file is part of BeRTOS.
+ *
+ * Bertos is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, you may use this file as part of a free software
+ * library without restriction.  Specifically, if other files instantiate
+ * templates or use macros or inline functions from this file, or you compile
+ * this file and link it with other files to produce an executable, this
+ * file does not by itself cause the resulting executable to be covered by
+ * the GNU General Public License.  This exception does not however
+ * invalidate any other reasons why the executable file might be covered by
+ * the GNU General Public License.
+ *
  * Copyright 2007 Develer S.r.l. (http://www.develer.com/)
  * -->
- *
- * \version $Id: pocketcmd.c 16587 2007-10-02 14:31:02Z batt $
- *
- * \author Francesco Sacchi <batt@develer.com>
  *
  * \brief pocketBus protocol Command layer implementation.
  *
@@ -28,6 +49,10 @@
  *
  * The CMD ID used is the same supplied by the master when
  * the command was sent.
+ *
+ * \version $Id: pocketcmd.c 16587 2007-10-02 14:31:02Z batt $
+ *
+ * \author Francesco Sacchi <batt@develer.com>
  */
 
 #include "pocketcmd.h"
