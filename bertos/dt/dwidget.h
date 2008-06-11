71bc957cd6feffc26a711e21b1638ea3214a3a0e
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-06-11 17:16:34 +0000

    Remove CVS logs.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1447 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/dt/dwidget.h
+++ bertos/dt/dwidget.h
@@ -38,25 +38,6 @@
  * \author Francesco Sacchi <batt@develer.com>
  */
 
-/*#*
- *#* $Log$
- *#* Revision 1.2  2006/07/19 12:56:26  bernie
- *#* Convert to new Doxygen style.
- *#*
- *#* Revision 1.1  2005/11/04 18:26:38  bernie
- *#* Import into DevLib.
- *#*
- *#* Revision 1.3  2005/06/06 11:04:12  batt
- *#* Add some comments.
- *#*
- *#* Revision 1.2  2005/05/26 14:44:10  batt
- *#* Abstract widget from layer: use context.
- *#*
- *#* Revision 1.1  2005/05/26 08:32:53  batt
- *#* Add new Develer widget system :)
- *#*
- *#*/
-
 #ifndef DT_DWIDGET_H
 #define DT_DWIDGET_H
 
