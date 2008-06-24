5eb2e1bf90a38f1e083186f42b4fef43ad9427c6
qwert <qwert@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-06-24 14:29:22 +0000

    Add missing semicolon
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1483 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/rules.mk
+++ bertos/rules.mk
@@ -275,7 +275,7 @@ bumprev:
 		buildnr=`sed <"$(BUILDREV_H)" -n -e 's/#define VERS_BUILD \([0-9][0-9]*\)/\1/p'`; \
 	fi; \
 	buildnr=`expr $$buildnr + 1`; \
-	buildhost=`hostname | sed -n -e '1h;2,$$H;$${g;s/\n//g;p}'`; \
+	buildhost=`hostname | sed -n -e '1h;2,$$H;$${g;s/\n//g;p;}'`; \
 	echo "#define VERS_BUILD $$buildnr" >"$(BUILDREV_H)"; \
 	echo "#define VERS_HOST  \"$$buildhost\"" >>"$(BUILDREV_H)"; \
 	echo "Building revision $$buildnr"
