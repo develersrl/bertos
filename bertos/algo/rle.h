3ce6434919d7607917e366a7b69fcbfb918d534b
bernie <bernie@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-08-13 09:45:31 +0000

    Move algorithms from mware/ to algo/
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1627 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/algo/rle.h
+++ bertos/algo/rle.h
@@ -26,17 +26,20 @@
  * invalidate any other reasons why the executable file might be covered by
  * the GNU General Public License.
  *
- * Copyright 2005 Develer S.r.l. (http://www.develer.com/)
- * -->
+ * Copyright 2004 Develer S.r.l. (http://www.develer.com/)
+ * Copyright 1999, 2000, 2001 Bernie Innocenti <bernie@codewiz.org>
  *
- * \brief Poor man's hex arrays (implementation).
+ * -->
  *
  * \version $Id$
  * \author Bernie Innocenti <bernie@codewiz.org>
+ *
+ * \brief General-purpose run-length {en,de}coding algorithm (interface)
  */
+#ifndef RLE_H
+#define RLE_H
 
+int rle(unsigned char *output, const unsigned char *input, int length);
+int unrle(unsigned char *output, const unsigned char *input);
 
-#include "hex.h"
-
-const char hex_tab[16] = { '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f' };
-const char HEX_tab[16] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };
+#endif /* RLE_H */
