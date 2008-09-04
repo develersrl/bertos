9bf7de00de04e8d690f309fce6e3892599a478b1
qwert <qwert@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-09-04 16:52:11 +0000

    Start to develop a dummy app for Luminary Micro Stellaris. It need a startup routine..coming soon.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1787 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ app/lm3s1968/lm3s1968.mk
@@ -0,0 +1,31 @@
+#
+# $Id: lm3s1968.mk 18234 2007-10-08 13:39:48Z rasky $
+# Copyright 2006 Develer S.r.l. (http://www.develer.com/)
+# All rights reserved.
+#
+# Makefile fragment for DevLib lm3s1968 application.
+#
+# Author: Manuele Fanelli <qwert@develer.com>
+#
+#
+
+# Set to 1 for debug builds
+lm3s1968_DEBUG = 0
+
+
+# Our target application
+TRG += lm3s1968
+
+lm3s1968_CSRC = \
+	app/lm3s1968/lm3s1968.c \
+	#bertos/cpu/cortex-m3/startup_lm3s.c
+
+
+lm3s1968_CROSS = arm-none-eabi-
+
+lm3s1968_CPPAFLAGS = -O0 -g -gdwarf-2 -g -gen-debug -mthumb
+lm3s1968_CPPFLAGS = -O0 -D'ARCH=0' -D__ARM_LM3S1968__ -g3 -gdwarf-2 -fverbose-asm -mthumb  -Iapp/lm3s1968 -Ibertos/cpu/cortex-m3
+lm3s1968_LDFLAGS = -nostartfiles -T app/lm3s1968/standalone.ld -Wl,--no-warn-mismatch
+
+lm3s1968_CPU = cortex-m3
+
