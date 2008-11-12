2c60cf261046a474553ccdf2c4c3f3b073066544
batt <batt@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-11-12 21:41:15 +0000

    Disable priority and preemption.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1931 38d2e660-2303-0410-9eaa-f027e97ec537

--- examples/demo/cfg/cfg_kern.h
+++ examples/demo/cfg/cfg_kern.h
@@ -50,12 +50,12 @@
  */
 #define CONFIG_KERN_SCHED       1  ///< Process schedling
 #define CONFIG_KERN_SIGNALS     1  ///< Inter-process signals
-#define CONFIG_KERN_IRQ         1  ///< Interrupt supervisor
+#define CONFIG_KERN_IRQ         0  ///< Interrupt supervisor
 #define CONFIG_KERN_HEAP        0  ///< Dynamic memory allocation
-#define CONFIG_KERN_SEMAPHORES  0  ///< Re-entrant mutual exclusion primitives
+#define CONFIG_KERN_SEMAPHORES  1  ///< Re-entrant mutual exclusion primitives
 #define CONFIG_KERN_MONITOR     1  ///< Process monitor
-#define CONFIG_KERN_PREEMPT     1  ///< Preemptive process scheduling
-#define CONFIG_KERN_PRI         1  ///< Priority-based scheduling policy
+#define CONFIG_KERN_PREEMPT     0  ///< Preemptive process scheduling
+#define CONFIG_KERN_PRI         0  ///< Priority-based scheduling policy
 /*\}*/
 
 /// [ms] Time sharing quantum (a prime number prevents interference effects)
