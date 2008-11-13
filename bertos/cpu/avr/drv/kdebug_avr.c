0f2a55976024328d266b3349cc011ea02ce82c8c
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-11-13 11:43:12 +0000

    Add debug support for atmega1281.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1933 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/cpu/avr/drv/kdebug_avr.c
+++ bertos/cpu/avr/drv/kdebug_avr.c
@@ -73,7 +73,7 @@
 	#define KDBG_UART0_BUS_TX    do {} while (0)
 	#endif
 
-	#if CPU_AVR_ATMEGA64 || CPU_AVR_ATMEGA128 || CPU_AVR_ATMEGA168
+	#if CPU_AVR_ATMEGA64 || CPU_AVR_ATMEGA128 || CPU_AVR_ATMEGA1281 || CPU_AVR_ATMEGA168
 		#define UCR UCSR0B
 		#define UDR UDR0
 		#define USR UCSR0A
@@ -232,7 +232,7 @@ INLINE void kdbg_hw_init(void)
 		/* Compute the baud rate */
 		uint16_t period = DIV_ROUND(CLOCK_FREQ / 16UL, CONFIG_KDEBUG_BAUDRATE) - 1;
 
-		#if (CPU_AVR_ATMEGA64 || CPU_AVR_ATMEGA128)
+		#if (CPU_AVR_ATMEGA64 || CPU_AVR_ATMEGA128 || CPU_AVR_ATMEGA1281)
 			#if CONFIG_KDEBUG_PORT == 0
 				UBRR0H = (uint8_t)(period>>8);
 				UBRR0L = (uint8_t)period;
