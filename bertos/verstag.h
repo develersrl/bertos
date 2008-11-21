e49437c15d4af19661157ede2c69db9811e3da3d
batt <batt@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-11-21 12:00:33 +0000

    Revert bad commit.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1941 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/verstag.h
+++ bertos/verstag.h
@@ -62,7 +62,7 @@
 	#define VERS_MINOR 1
 	#define VERS_REV   0
 	#define VERS_LETTER ""
-#elif ((ARCH & ARCH_NIGHTTEST) || (ARCH & ARCH_UNITTEST))
+#elif ARCH & ARCH_NIGHTTEST
 	#define VERS_MAJOR 0
 	#define VERS_MINOR 1
 	#define VERS_REV   0
