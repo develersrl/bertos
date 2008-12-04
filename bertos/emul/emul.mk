4c1123f234a975a1c041054ea183e5b0bd20015a
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-12-04 17:13:54 +0000

    Added support for Mac Os X compilation without pkg-config utility.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1973 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/emul/emul.mk
+++ bertos/emul/emul.mk
@@ -7,8 +7,16 @@
 # Qt configuration for BeRTOS emulator.  Include it from
 # your app Makefile fragment
 
-EMUL_CFLAGS = -D'ARCH=(ARCH_EMUL|ARCH_QT)' \
-	$(shell pkg-config QtCore QtGui --cflags) -DQT_CLEAN_NAMESPACE
-EMUL_LDFLAGS = $(shell pkg-config QtCore QtGui --libs)
-QT_MOC = $(shell pkg-config QtCore --variable=moc_location)
-#QT_UIC = $(shell pkg-config QtCore --variable=uic_location)
+#On Darwin architecture environment variables need a different configuration.
+ifeq ($(shell uname | grep -c "Darwin"),1)
+	EMUL_CFLAGS = -D'ARCH=(ARCH_EMUL|ARCH_QT)' -DQT_CLEAN_NAMESPACE
+	EMUL_LDFLAGS = -framework QtGui -framework QtCore
+	QT_MOC = /usr/bin/moc
+#On other platforms.
+else
+	EMUL_CFLAGS = -D'ARCH=(ARCH_EMUL|ARCH_QT)' $(shell pkg-config QtCore QtGui --cflags) -DQT_CLEAN_NAMESPACE
+	EMUL_LDFLAGS = $(shell pkg-config QtCore QtGui --libs)
+	QT_MOC = $(shell pkg-config QtCore --variable=moc_location)
+	#QT_UIC = $(shell pkg-config QtCore --variable=uic_location)
+endif
+
