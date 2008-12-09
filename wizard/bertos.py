41444fbd11857041fb3646491987ba8f93f0083e
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-12-09 18:13:49 +0000

    Add stub of main script
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1980 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ wizard/bertos.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright 2008 Develer S.r.l. (http://www.develer.com/)
+# All rights reserved.
+#
+# $Id:$
+#
+# Author: Lorenzo Berni <duplo@develer.com>
+#
+
+import sys
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
