e3d37f4b700581ed5e890e7cf8f165431e665179
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-12-10 17:22:42 +0000

    Add the _setupUi method for hiding unused widget
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1995 38d2e660-2303-0410-9eaa-f027e97ec537

--- wizard/BVersionPage.py
+++ wizard/BVersionPage.py
@@ -16,7 +16,12 @@ class BVersionPage(BWizardPage):
     def __init__(self):
         BWizardPage.__init__(self, "bertos_versions.ui")
         self._connectSignals()
+        self._setupUi()
         self.setTitle(self.tr("Select the beRTOS version needed"))
     
     def _connectSignals(self):
         pass
+    
+    def _setupUi(self):
+        self.pageContent.updateProgressBar.setVisible(False)
+        self.pageContent.statusLabel.setVisible(False)
