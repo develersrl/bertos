6e76a376f926ae0cf878eef619bb85240b8eaf03
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-12-10 15:14:29 +0000

    Add *.pyc in svn:ignore
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1987 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ wizard/BWizardPage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright 2008 Develer S.r.l. (http://www.develer.com/)
+# All rights reserved.
+#
+# $Id:$
+#
+# Author: Lorenzo Berni <duplo@develer.com>
+#
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4 import uic
+
+class BWizardPage(QWizardPage):
+    
+    def __init__(self, wizardGui, parent = None):
+        QWizardPage.__init__(self, parent)
+        uic.loadUiType("start.ui")
+        self.pageContent = uic.loadUi(wizardGui, None)
+        layout = QVBoxLayout()
+        layout.addWidget(self.pageContent)
+        self.setLayout(layout)
