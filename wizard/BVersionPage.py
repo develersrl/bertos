53ff40ff733a4d2b87ccae2c004077303c7e2e43
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-12-10 17:24:35 +0000

    Add messages on button click
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@1996 38d2e660-2303-0410-9eaa-f027e97ec537

--- wizard/BVersionPage.py
+++ wizard/BVersionPage.py
@@ -20,8 +20,11 @@ class BVersionPage(BWizardPage):
         self.setTitle(self.tr("Select the beRTOS version needed"))
     
     def _connectSignals(self):
-        pass
+        self.connect(self.pageContent.updateButton, SIGNAL("clicked()"), self.updateClicked))
     
     def _setupUi(self):
         self.pageContent.updateProgressBar.setVisible(False)
         self.pageContent.statusLabel.setVisible(False)
+    
+    def updateClicked(self):
+        print "fake update checking"
