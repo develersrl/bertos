8434e4eb9b35946ab8d01b0312100dde827be912
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-12-12 11:05:17 +0000

    Add shell script for generate the wizard resource
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2024 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ wizard/build_resources.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# encoding: utf-8
+#
+# Copyright 2008 Develer S.r.l. (http://www.develer.com/)
+# All rights reserved.
+#
+# $Id:$
+#
+# Author: Lorenzo Berni <duplo@develer.com>
+#
+
+rcc -binary bertos.qrc -o bertos.rcc
