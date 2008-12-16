c9548deb3275b044cd5f4fbc941598a7a442b836
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-12-16 14:55:22 +0000

    Move the start page in a separate QWidget and remove it from the QWizard
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2045 38d2e660-2303-0410-9eaa-f027e97ec537

--- wizard/BStartPage.py
+++ wizard/BStartPage.py
@@ -9,47 +9,39 @@
 # Author: Lorenzo Berni <duplo@develer.com>
 #
 
-from BWizardPage import *
-import bertos_utils
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import PyQt4.uic as uic
 
-class BStartPage(BWizardPage):
+class BStartPage(QWidget):
     
     def __init__(self):
-        BWizardPage.__init__(self, "start.ui")
-        self._connectSignals()
-        self.setTitle(self.tr("Create or edit a BeRTOS project"))
-        self._initializeButtons()
+        QDialog.__init__(self)
         self._setupUi()
-        
+        self._connectSignals()
+        self.setWindowTitle(self.tr("Create or edit a BeRTOS project"))
+        self._initializeButtons()
+    
+    def _setupUi(self):
+        self.content = uic.loadUi("start.ui", None)
+        layout = QVBoxLayout()
+        layout.addWidget(self.content)
+        self.setLayout(layout)
+    
     def _connectSignals(self):
-        self.connect(self.pageContent.newButton, SIGNAL("clicked()"), self.newProject)
-        self.connect(self.pageContent.editButton, SIGNAL("clicked()"), self.editProject)
+        self.connect(self.content.newButton, SIGNAL("clicked()"), self.newProject)
+        self.connect(self.content.editButton, SIGNAL("clicked()"), self.editProject)
     
     def _initializeButtons(self):
         self.buttonGroup = QButtonGroup()
-        self.buttonGroup.addButton(self.pageContent.newButton)
-        self.buttonGroup.addButton(self.pageContent.editButton)
+        self.buttonGroup.addButton(self.content.newButton)
+        self.buttonGroup.addButton(self.content.editButton)
         self.buttonGroup.setExclusive(True)
-
-    def _setupUi(self):
-        self.pageContent.newDescription.setVisible(False)
-        self.pageContent.editDescription.setVisible(False)
-    
-    def isComplete(self):
-        return self.pageContent.newButton.isChecked() or self.pageContent.editButton.isChecked()
-    
+        
     def newProject(self):
-        filename = QFileDialog.getSaveFileName(self, self.tr("Destination directory"), "", "", "", QFileDialog.ShowDirsOnly)
-        if not filename.isEmpty():
-            self.pageContent.newDescription.setText("(\"" + filename + "\")")
-            self.pageContent.newDescription.setVisible(True)
-            # TODO: It's better to create it at the end of the wizard...
-            bertos_utils.createBertosProject(filename)
-            self.emit(SIGNAL("completeChanged()"))
-        else:
-            self.pageContent.newDescription.setText("")
-            self.pageContent.newDescription.setVisible(False)
-            self.pageContent.newButton.setChecked(False)
+        self.emit(SIGNAL("newProject"))
+        self.close()
     
     def editProject(self):
-        self.pageContent.newButton.setChecked(False)
+        self.emit(SIGNAL("editProject"))
+        self.close()
