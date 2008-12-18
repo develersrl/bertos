7a132bfa3128cc7f34a7351a15d0e99211cf1c64
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-12-18 11:49:06 +0000

    Add a stub of the cpu list populate method
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2075 38d2e660-2303-0410-9eaa-f027e97ec537

--- wizard/BCpuPage.py
+++ wizard/BCpuPage.py
@@ -9,11 +9,19 @@
 # Author: Lorenzo Berni <duplo@develer.com>
 #
 
+
 from BWizardPage import *
+import bertos_utils
 
 class BCpuPage(BWizardPage):
     
     def __init__(self):
         BWizardPage.__init__(self, "cpu_select.ui")
         self.setTitle(self.tr("Select the CPU"))
+        #self._populateCpuList()
+    
+    def _populateCpuList(self):
+        cpus = bertos_utils.findDefinitions("cdef", self._projectInfoRetrieve("SOURCES_PATH"))
+        for cpu in cpus.keys():
+            self.pageContent.cpuList.addItem(QListWidgetItem(cpu))
         
