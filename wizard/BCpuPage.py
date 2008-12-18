d07bfc9a7ed35125525f82b331d8dab25f06fca8
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-12-18 14:20:47 +0000

    The cpu list selection page show the cpu definition file names
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2083 38d2e660-2303-0410-9eaa-f027e97ec537

--- wizard/BCpuPage.py
+++ wizard/BCpuPage.py
@@ -18,10 +18,12 @@ class BCpuPage(BWizardPage):
     def __init__(self):
         BWizardPage.__init__(self, "cpu_select.ui")
         self.setTitle(self.tr("Select the CPU"))
-        #self._populateCpuList()
     
     def _populateCpuList(self):
         cpus = bertos_utils.findDefinitions("cdef", self._projectInfoRetrieve("SOURCES_PATH"))
         for cpu in cpus.keys():
             self.pageContent.cpuList.addItem(QListWidgetItem(cpu))
+    
+    def reloadData(self):
+        self._populateCpuList()
         
