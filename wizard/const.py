322a49d2595b4709c7206f38e06f9b02696f1273
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-12-19 11:16:43 +0000

    Add a file containing the wizard constants
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2101 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ wizard/const.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright 2008 Develer S.r.l. (http://www.develer.com/)
+# All rights reserved.
+#
+# $Id:$
+#
+# Author: Lorenzo Berni <duplo@develer.com>
+#
+
+CPU_DEF = {
+    "TOOLCHAIN": [],
+    "CORE_CPU": "",
+    "SCRIPT_DIR": "",
+    "HW_DIR": "",
+    "DRV_DIR": "",
+    "CPPA_FLAGS" : [],
+    "CPP_FLAGS" : [],
+    "LD_FLAGS" : [],
+    "CPPA_SRC" : [],
+    "SUPPORTED_DRV" : [],
+    "PC_SRC" : [],
+    "CPU_DESC" : []
+}
+
+CPU_DEFINITION = "cdef"
