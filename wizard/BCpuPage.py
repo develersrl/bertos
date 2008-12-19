fc1ec20eecfc1c644205002f14a3fcbf83fe7c9c
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2008-12-19 15:45:11 +0000

    Add the cpu descrpition in the cpu select page
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2114 38d2e660-2303-0410-9eaa-f027e97ec537

--- wizard/BCpuPage.py
+++ wizard/BCpuPage.py
@@ -19,12 +19,17 @@ class BCpuPage(BWizardPage):
         BWizardPage.__init__(self, "cpu_select.ui")
         self.setTitle(self.tr("Select the CPU"))
         self._connectSignals()
+        self._setupUi()
     
     def _populateCpuList(self):
         self.pageContent.cpuList.clear()
         infos = bertos_utils.loadCpuInfos(self._projectInfoRetrieve("SOURCES_PATH"))
         for cpu in infos:
-            self.pageContent.cpuList.addItem(QListWidgetItem(cpu["CPU_NAME"]))
+            item = QListWidgetItem(cpu["CPU_NAME"])
+            # The CPU_DESC field in the cpu definition is a list of string, so we need to 
+            # store it as a QStringList in a QVariant
+            item.setData(Qt.UserRole, QVariant(QStringList(cpu["CPU_DESC"])))
+            self.pageContent.cpuList.addItem(item)
     
     def _connectSignals(self):
         self.connect(self.pageContent.cpuList, SIGNAL("itemSelectionChanged()"), self.rowChanged)
@@ -33,7 +38,10 @@ class BCpuPage(BWizardPage):
         elements = self.pageContent.cpuList.findItems(cpu, Qt.MatchCaseSensitive)
         if len(elements) == 1:
             self.pageContent.cpuList.setCurrentItem(elements[0])
-
+    
+    def _setupUi(self):
+        self.pageContent.descriptionLabel.setVisible(False)
+    
     def reloadData(self):
         self._populateCpuList()
         cpuName = self._projectInfoRetrieve("CPU_NAME")
@@ -48,4 +56,9 @@ class BCpuPage(BWizardPage):
             return False
         
     def rowChanged(self):
+        description = self.pageContent.cpuList.currentItem().data(Qt.UserRole).toStringList()
+        # We need to convert the list of QString in a list of unicode
+        description =  [unicode(line) for line in description]
+        self.pageContent.descriptionLabel.setText("<br>".join(description))
+        self.pageContent.descriptionLabel.setVisible(True)
         self.emit(SIGNAL("completeChanged()"))
