c0e35b5a79c55660f10362a5af6298f94fffef56
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2009-01-30 15:08:27 +0000

    Add a template of the project .mk file, with keywords instead of values, ready to be replaced by the values
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2242 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ wizard/template.mk
@@ -0,0 +1,33 @@
+#
+# Copyright 2009 Develer S.r.l. (http://www.develer.com/)
+# All rights reserved.
+#
+# Makefile template for BeRTOS wizard.
+#
+# Author: Lorenzo Berni <duplo@develer.com>
+#
+#
+
+# Set to 1 for debug builds
+pname_DEBUG = 1
+
+# Our target application
+TRG += pname
+CPU = cpuname
+BOOT_ADDR_START = 0x1E000
+
+pname_PREFIX = prefix
+
+pname_SUFFIX = suffix
+
+pname_MCU = $(CPU)
+
+pname_CSRC = \
+	csrc
+	#
+
+pname_PCSRC += pcsrc
+
+
+pname_CFLAGS = cflags
+pname_LDFLAGS = ldflags
