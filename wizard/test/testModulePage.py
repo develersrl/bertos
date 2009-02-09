170e5a32acbf87808fd5c92153a59d50b3c3b88d
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2009-02-09 09:02:12 +0000

    Add the test for the Module page, useful when someone need to test the wizard configurations in the header files, without run the entire wizard.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2324 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ wizard/test/testModulePage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright 2009 Develer S.r.l. (http://www.develer.com/)
+# All rights reserved.
+#
+# $Id:$
+#
+# Author: Lorenzo Berni <duplo@develer.com>
+#
+
+import sys
+import os
+
+from PyQt4.QtGui import *
+
+# Add the wizard class path in the path for the testing module
+sys.path.append("../")
+
+import BProject
+import BModulePage
+
+# Add a custom UI_LOCATION constant
+BModulePage.UI_LOCATION = "../ui"
+
+def main():
+    app = QApplication([])
+    app.project = BProject.BProject()
+    page = BModulePage.BModulePage()
+    page._projectInfoStore("SOURCES_PATH", "../../")
+    page.reloadData()
+    page.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
+
