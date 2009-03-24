e312e0e25be4b0abddfa466770e0108d84437b82
batt <batt@38d2e660-2303-0410-9eaa-f027e97ec537>
2009-03-24 10:49:29 +0000

    Remove the trailing separator.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2407 38d2e660-2303-0410-9eaa-f027e97ec537

--- wizard/BVersionPage.py
+++ wizard/BVersionPage.py
@@ -64,8 +64,12 @@ class BVersionPage(BWizardPage):
 
     def isComplete(self):
         if self.pageContent.versionList.currentRow() != -1:
-            self._projectInfoStore("SOURCES_PATH", qvariant_converter.getString(self.pageContent.versionList.currentItem().data(Qt.UserRole)))
-            return True
+	    # Remove trailing slash
+	    sources_path = qvariant_converter.getString(self.pageContent.versionList.currentItem().data(Qt.UserRole))
+	    if sources_path.endswith(os.sep):
+                 sources_path = sources_path[:-1]
+            self._projectInfoStore("SOURCES_PATH", sources_path)
+	    return True
         else:
             return False
     
