a01c6b3277e3c0d5c327824dbab22b1f4e92d8ad
batt <batt@38d2e660-2303-0410-9eaa-f027e97ec537>
2009-04-20 09:52:56 +0000

    Fix phase driver configuration.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2565 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/cfg/cfg_phase.h
+++ bertos/cfg/cfg_phase.h
@@ -39,10 +39,18 @@
 #ifndef CFG_PHASE_H
 #define CFG_PHASE_H
 
-/// Max value of the duty cycle on triac. $WIZ$ type = "int"
+/**
+ * Max value of the duty cycle on triac.
+ * $WIZ$ type = "int"
+ * $WIZ$ min = "1"
+ */
 #define CONFIG_TRIAC_MAX_DUTY  100
 
-/// Max value of the triac power. $WIZ$ type = "int"
+/**
+ * Max value of the triac power.
+ * $WIZ$ type = "int"
+ * $WIZ$ min = "1"
+ */
 #define CONFIG_TRIAC_MAX_POWER 100
 
 #endif /* CFG_PHASE_H */
