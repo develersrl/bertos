ff9e3c69aa5e11915f056f45f6200009dd29127c
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2009-04-22 18:00:10 +0000

    Removed dynamic plugin system
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2641 38d2e660-2303-0410-9eaa-f027e97ec537

--- wizard/plugins/__init__.py
+++ wizard/plugins/__init__.py
@@ -8,18 +8,6 @@
 #
 # Author: Lorenzo Berni <duplo@develer.com>
 #
+import codelite
 
-import os
-import glob
-
-_modules = glob.glob(os.path.dirname(os.path.abspath(__file__)) + os.sep + "*.py")
-_modules = [os.path.basename(module) for module in _modules]
-__all__ = []
-for module in _modules:
-    if module != "__init__.py":
-        __all__.append(module.replace(".py", ""))
-
-del os
-del glob
-del _modules
-del module
+__all__ = ["codelite"]
