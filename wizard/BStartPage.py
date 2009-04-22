f3c5a7fbce17712e088c3d9f9719e42d8ba7fff3
batt <batt@38d2e660-2303-0410-9eaa-f027e97ec537>
2009-04-22 11:12:16 +0000

    Show wizard version.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2623 38d2e660-2303-0410-9eaa-f027e97ec537

--- wizard/BStartPage.py
+++ wizard/BStartPage.py
@@ -4,7 +4,7 @@
 # Copyright 2008 Develer S.r.l. (http://www.develer.com/)
 # All rights reserved.
 #
-# $Id:$
+# $Id$
 #
 # Author: Lorenzo Berni <duplo@develer.com>
 #
@@ -14,37 +14,38 @@ from PyQt4.QtGui import *
 import PyQt4.uic as uic
 
 from const import *
+from version import wizard_version
 
 class BStartPage(QDialog):
-    
+
     def __init__(self):
         QDialog.__init__(self)
         self.setupUi()
         self.connectSignals()
-        self.setWindowTitle(self.tr("Create or edit a BeRTOS project"))
+        self.setWindowTitle(self.tr("Create or edit a BeRTOS project - rev.%1").arg(wizard_version))
         self.initializeButtons()
-    
+
     def setupUi(self):
         self.content = uic.loadUi(UI_LOCATION + "/start.ui", None)
         self.setWindowIcon(QIcon(":/images/appicon.png"))
         layout = QVBoxLayout()
         layout.addWidget(self.content)
         self.setLayout(layout)
-    
+
     def connectSignals(self):
         self.connect(self.content.newButton, SIGNAL("clicked()"), self.newProject)
         self.connect(self.content.editButton, SIGNAL("clicked()"), self.editProject)
-    
+
     def initializeButtons(self):
         self.button_group = QButtonGroup()
         self.button_group.addButton(self.content.newButton)
         self.button_group.addButton(self.content.editButton)
         self.button_group.setExclusive(True)
-        
+
     def newProject(self):
         self.close()
         self.emit(SIGNAL("newProject"))
-    
+
     def editProject(self):
         self.close()
         self.emit(SIGNAL("editProject"))
---
+++ wizard/version.py
@@ -0,0 +1,1 @@
+wizard_version = "0000"
