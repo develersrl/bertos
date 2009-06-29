dee2d8a24e84bec51a3dec2b4b2955517c254c1d
lottaviano <lottaviano@38d2e660-2303-0410-9eaa-f027e97ec537>
2009-06-29 12:56:26 +0000

    doc: Add CHM builder script and Doxyfile.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2720 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ doc/chm-builder.py
@@ -0,0 +1,38 @@
+"""
+To be used on windows platform only to generate BeRTOS .chm documentation.
+Requires doxygen.exe and hhc.exe to be in the system PATH.
+"""
+from __future__ import with_statement
+import os, sys
+
+def move(old_path, new_path):
+    if os.path.exists(new_path):
+        os.unlink(new_path)
+    os.rename(old_path, new_path)
+
+
+if sys.platform != 'win32':
+    print "This program can be run only on a Windows machine"
+    sys.exit(-1)
+
+if len(sys.argv) < 1:
+    print "Usage: " + sys.argv[0] + " [custom_doxyfile]"
+    sys.exit(-1)
+
+toc_modifier = r"doc\chm-toc-modifier.py"
+toc_file = r"doc\reference\html\index.hhc"
+bertos_toc = r"bertos-toc.hhc"
+
+if os.system("doxygen " + sys.argv[1]):
+    print "doxygen error"
+if os.system(toc_modifier + " " + toc_file + " " + bertos_toc):
+    print "toc-modifier error"
+move(bertos_toc, toc_file)
+
+# compile CHM
+os.system(r"hhc doc\reference\html\index.hhp")
+
+chm_target = r"doc\bertos-doc.chm"
+move(r"doc\reference\html\bertos-doc.chm", chm_target)
+
+
