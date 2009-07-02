cad89d93709f2dd4dc0296d87c07735717839790
lottaviano <lottaviano@38d2e660-2303-0410-9eaa-f027e97ec537>
2009-07-02 11:03:58 +0000

    doc: Change offline documentation path
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2729 38d2e660-2303-0410-9eaa-f027e97ec537

--- doc/chm-builder.py
+++ doc/chm-builder.py
@@ -15,13 +15,15 @@ if sys.platform != 'win32':
     print "This program can be run only on a Windows machine"
     sys.exit(-1)
 
-if len(sys.argv) < 1:
+if len(sys.argv) < 2:
     print "Usage: " + sys.argv[0] + " [custom_doxyfile]"
     sys.exit(-1)
 
+DOC_PATH = 'doc\\offline-reference\\html\\'
 toc_modifier = r"doc\chm-toc-modifier.py"
-toc_file = r"doc\reference\html\index.hhc"
+toc_file = DOC_PATH + r"index.hhc"
 bertos_toc = r"bertos-toc.hhc"
+
 
 if os.system("doxygen " + sys.argv[1]):
     print "doxygen error"
@@ -30,9 +32,9 @@ if os.system(toc_modifier + " " + toc_file + " " + bertos_toc):
 move(bertos_toc, toc_file)
 
 # compile CHM
-os.system(r"hhc doc\reference\html\index.hhp")
+os.system(r"hhc " + DOC_PATH + "index.hhp")
 
 chm_target = r"doc\bertos-doc.chm"
-move(r"doc\reference\html\bertos-doc.chm", chm_target)
+move(DOC_PATH + r"bertos-doc.chm", chm_target)
 
 
