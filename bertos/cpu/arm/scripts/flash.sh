813c64a4e34c51c6319934e7d12de4c3df62e6fd
(no author) <(no author)@38d2e660-2303-0410-9eaa-f027e97ec537>
2009-08-27 12:57:09 +0000

    Remove trace; add debug info.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2793 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/cpu/arm/scripts/flash.sh
+++ bertos/cpu/arm/scripts/flash.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-set -x
+#set -x
 
 BASE_DIR=`dirname $0`
 INT_FILE=${BASE_DIR}/openocd/${PROGRAMMER_TYPE}.cfg 
@@ -19,7 +19,7 @@ fi
 sed -e "s#PROGRAMMER_TYPE#${INT_FILE}#" ${BASE_DIR}/openocd/flash.cfg | sed -e "s#PROGRAMMER_CPU#${CPU_FILE}#" \
 	| sed -e "s#IMAGE_FILE#${IMAGE_FILE}#" > ${OUT_FILE}
 
-openocd -f ${OUT_FILE}
+openocd -d 1 -f ${OUT_FILE}
 OCD_RET=$?
 rm -f ${OUT_FILE}
 exit $OCD_RET
