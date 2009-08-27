4b94981fcfe9e893527ed913fe903902be1d2c4a
lottaviano <lottaviano@38d2e660-2303-0410-9eaa-f027e97ec537>
2009-08-27 09:17:49 +0000

    Update 'make flash' support for ARM chips.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2788 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/cpu/arm/scripts/flash.sh
+++ bertos/cpu/arm/scripts/flash.sh
@@ -2,8 +2,9 @@
 set -x
 
 BASE_DIR=`dirname $0`
-INT_FILE=${BASE_DIR}/openocd/${PROGRAMMER_TYPE}.tcl 
-CPU_FILE=${BASE_DIR}/openocd/${PROGRAMMER_CPU}.tcl 
+INT_FILE=${BASE_DIR}/openocd/${PROGRAMMER_TYPE}.cfg 
+CPU_FILE=${BASE_DIR}/openocd/${PROGRAMMER_CPU}.cfg 
+OUT_FILE="openocd.tmp"
 
 if [ ! -f ${INT_FILE} ]; then
 	printf "Interface ${PROGRAMMER_TYPE} not supported\n";
@@ -15,4 +16,10 @@ if [ ! -f ${CPU_FILE} ]; then
 	exit 1;
 fi
 
-sed -e "s#PROGRAMMER_TYPE#${INT_FILE}#" ${BASE_DIR}/openocd/flash.cfg | sed -e "s#PROGRAMMER_CPU#${CPU_FILE}#" > openocd.tmp
+sed -e "s#PROGRAMMER_TYPE#${INT_FILE}#" ${BASE_DIR}/openocd/flash.cfg | sed -e "s#PROGRAMMER_CPU#${CPU_FILE}#" \
+	| sed -e "s#IMAGE_FILE#${IMAGE_FILE}#" > ${OUT_FILE}
+
+openocd -f ${OUT_FILE}
+OCD_RET=$?
+rm -f ${OUT_FILE}
+exit $OCD_RET
