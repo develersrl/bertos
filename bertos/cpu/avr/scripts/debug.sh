d82169c5d84ea8b5b81e4625dea7b838dffb777e
batt <batt@38d2e660-2303-0410-9eaa-f027e97ec537>
2009-08-28 09:48:19 +0000

    Remove scripts.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2808 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/cpu/avr/scripts/debug.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-printf "AVR target debugging is currently not supported.\n"
-exit 1
--- bertos/cpu/avr/scripts/flash.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-avrdude -p ${PROGRAMMER_CPU} -c ${PROGRAMMER_TYPE} -P ${PROGRAMMER_PORT} -U ${IMAGE_FILE}
