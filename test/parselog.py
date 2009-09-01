7a0f755229ff2a3f7b6fc1a4c874729b7bac26f4
batt <batt@38d2e660-2303-0410-9eaa-f027e97ec537>
2009-09-01 17:02:39 +0000

    Add log parser for nightly_test script.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@2854 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ test/parselog.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# This file is part of BeRTOS.
+#
+# Bertos is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# As a special exception, you may use this file as part of a free software
+# library without restriction.  Specifically, if other files instantiate
+# templates or use macros or inline functions from this file, or you compile
+# this file and link it with other files to produce an executable, this
+# file does not by itself cause the resulting executable to be covered by
+# the GNU General Public License.  This exception does not however
+# invalidate any other reasons why the executable file might be covered by
+# the GNU General Public License.
+#
+# Copyright 2008 Develer S.r.l. (http://www.develer.com/)
+#
+# Author Francesco Sacchi <batt@develer.com>
+
+import sys
+
+f = file(sys.argv[1])
+warn_suppress_cnt = 0
+for line in f:
+	if "__FILTER_NEXT_WARNING__" in line:
+		warn_suppress_cnt += 1
+	
+	if "warning" in line:
+		if warn_suppress_cnt == 0:
+			print line,
+		else:
+			warn_suppress_cnt -= 1
+
+	if "error" in line:
+		print line,
+
