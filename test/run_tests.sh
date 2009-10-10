75ffb2070346a144a9fa1c9301b789a7fc03f8fa
batt <batt@38d2e660-2303-0410-9eaa-f027e97ec537>
2009-10-10 09:09:35 +0000

    Add more warnings when compiling tests.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@3055 38d2e660-2303-0410-9eaa-f027e97ec537

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -17,7 +17,8 @@ VERBOSE=${VERBOSE:-1}
 
 CC=gcc
 #FIXME: -Ibertos/emul should not be needed
-CFLAGS="-W -Wall -Wextra -O0 -g3 -ggdb -Ibertos -Ibertos/emul -std=gnu99 -fno-builtin -D_DEBUG -DARCH=(ARCH_EMUL|ARCH_UNITTEST) \
+CFLAGS="-W -Wall -Wextra -Wundef -Wpointer-arith -Wcast-qual -Wcast-align -Wwrite-strings -Wsign-compare -Wmissing-noreturn \
+-O0 -g3 -ggdb -Ibertos -Ibertos/emul -std=gnu99 -fno-builtin -D_DEBUG -DARCH=(ARCH_EMUL|ARCH_UNITTEST) \
 -DCPU_FREQ=(12288000UL) -ffunction-sections -fdata-sections -Wl,--gc-sections"
 
 CXX=g++
