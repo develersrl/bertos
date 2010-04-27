5ff35a44ec40e775ba88f78ad32398bfdb916b39
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-04-27 20:29:24 +0000

    Add pwm register definitions.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@3554 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/cpu/cortex-m3/io/lm3s.h
+++ bertos/cpu/cortex-m3/io/lm3s.h
@@ -40,6 +40,8 @@
 #include <cfg/compiler.h>
 
 
+#define PWM_BASE     0x40028000UL
+
 #if CPU_CM3_LM3S1968
 	#include "lm3s1968.h"
 	#include "lm3s_types.h"
@@ -50,6 +52,7 @@
 	#include "lm3s_memmap.h"
 	#include "lm3s_uart.h"
 	#include "lm3s_ssi.h"
+	#include "lm3s_pwm.h"
 #else
 	#error Missing I/O definitions for CPU.
 #endif
