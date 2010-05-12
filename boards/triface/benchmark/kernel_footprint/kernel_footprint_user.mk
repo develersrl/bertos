84cf056f574c42a3bd577bf3929498acac7c897e
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-05-12 14:28:36 +0000

    Add triface kernel footprint benchmark.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@3666 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ boards/triface/benchmark/kernel_footprint/kernel_footprint_user.mk
@@ -0,0 +1,43 @@
+#
+# User makefile.
+# Edit this file to change compiler options and related stuff.
+#
+
+# Programmer interface configuration, see http://dev.bertos.org/wiki/ProgrammerInterface for help
+kernel_footprint_PROGRAMMER_TYPE = none
+kernel_footprint_PROGRAMMER_PORT = none
+
+# Files included by the user.
+kernel_footprint_USER_CSRC = \
+	$(kernel_footprint_SRC_PATH)/main.c \
+	#
+
+# Files included by the user.
+kernel_footprint_USER_PCSRC = \
+	#
+
+# Files included by the user.
+kernel_footprint_USER_CPPASRC = \
+	#
+
+# Files included by the user.
+kernel_footprint_USER_CXXSRC = \
+	#
+
+# Files included by the user.
+kernel_footprint_USER_ASRC = \
+	#
+
+# Flags included by the user.
+kernel_footprint_USER_LDFLAGS = \
+	#
+
+# Flags included by the user.
+kernel_footprint_USER_CPPAFLAGS = \
+	#
+
+# Flags included by the user.
+kernel_footprint_USER_CPPFLAGS = \
+	-fno-strict-aliasing \
+	-fwrapv \
+	#
