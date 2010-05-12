6542f8a6e9ce3289832a90b5f2518e05347b464b
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-05-12 14:07:23 +0000

    Add context switch benchmark for all boards.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@3663 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ boards/ek-lm3s1968/benchmarck/context_switch/.spec
@@ -0,0 +1,1 @@
+name = 'BeRTOS Context Switch'
---
+++ boards/triface/benchmark/context_switch/main.c
@@ -0,0 +1,56 @@
+
+/**
+ * \file
+ * <!--
+ * This file is part of BeRTOS.
+ *
+ * Bertos is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, you may use this file as part of a free software
+ * library without restriction.  Specifically, if other files instantiate
+ * templates or use macros or inline functions from this file, or you compile
+ * this file and link it with other files to produce an executable, this
+ * file does not by itself cause the resulting executable to be covered by
+ * the GNU General Public License.  This exception does not however
+ * invalidate any other reasons why the executable file might be covered by
+ * the GNU General Public License.
+ *
+ * Copyright 2010 Develer S.r.l. (http://www.develer.com/)
+ *
+ * -->
+ *
+ * \author Daniele Basile <asterix@develer.com>
+ *
+ * \brief Kernel switch context benchmark.
+ *
+ * This benchmark show you the switch context time. This measure is make
+ * by hp timer that measure accurately the kernel switch context time, and print
+ * it on serial debug. If you want not use the hp timer you can measure this
+ * time using one oscilloscope, connected to one board led. To use this
+ * mode you should disable hp timer and implement hw_led.h low level module.
+ */
+
+#include <benchmark/context_switch.h>
+
+int main(void)
+{
+	context_switch();
+	while (1)
+	{
+	}
+
+	return 0;
+}
+
