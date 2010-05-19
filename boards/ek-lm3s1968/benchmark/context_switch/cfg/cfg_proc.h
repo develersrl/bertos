215c1ef46da21472bbe51fad8a6395efbc55a3f0
arighi <arighi@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-05-19 12:57:22 +0000

    lm3s196: enable kernel preemption for the context_switch benchmark.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@3724 38d2e660-2303-0410-9eaa-f027e97ec537

--- boards/ek-lm3s1968/benchmark/context_switch/cfg/cfg_proc.h
+++ boards/ek-lm3s1968/benchmark/context_switch/cfg/cfg_proc.h
@@ -58,7 +58,7 @@
  * $WIZ$ type = "boolean"
  * $WIZ$ conditional_deps = "timer"
  */
-#define CONFIG_KERN_PREEMPT 0
+#define CONFIG_KERN_PREEMPT 1
 
 /**
  * Time sharing quantum (a prime number prevents interference effects) [ms].
