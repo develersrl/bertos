7f7ef05ca472e386e8b1c46373986981c58095fd
arighi <arighi@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-05-19 12:57:28 +0000

    lm3s1968: disable kernel heap support for the kernel footprint benchmark.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@3726 38d2e660-2303-0410-9eaa-f027e97ec537

--- boards/ek-lm3s1968/benchmark/kernel_footprint/cfg/cfg_proc.h
+++ boards/ek-lm3s1968/benchmark/kernel_footprint/cfg/cfg_proc.h
@@ -79,7 +79,7 @@
  * $WIZ$ type = "boolean"
  * $WIZ$ conditional_deps = "heap"
  */
-#define CONFIG_KERN_HEAP 1
+#define CONFIG_KERN_HEAP 0
 
 /**
  * Size of the dynamic memory pool used by processes.
