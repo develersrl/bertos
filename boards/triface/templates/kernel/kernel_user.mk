21a73d56f68ce7a0e3f44a41c466853982017582
arighi <arighi@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-05-25 16:47:24 +0000

    triface: add the "kernel" preset template.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@3836 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ boards/triface/templates/kernel/kernel_user.mk
@@ -0,0 +1,43 @@
+#
+# User makefile.
+# Edit this file to change compiler options and related stuff.
+#
+
+# Programmer interface configuration, see http://dev.bertos.org/wiki/ProgrammerInterface for help
+kernel_PROGRAMMER_TYPE = none
+kernel_PROGRAMMER_PORT = none
+
+# Files included by the user.
+kernel_USER_CSRC = \
+	$(kernel_SRC_PATH)/main.c \
+	#
+
+# Files included by the user.
+kernel_USER_PCSRC = \
+	#
+
+# Files included by the user.
+kernel_USER_CPPASRC = \
+	#
+
+# Files included by the user.
+kernel_USER_CXXSRC = \
+	#
+
+# Files included by the user.
+kernel_USER_ASRC = \
+	#
+
+# Flags included by the user.
+kernel_USER_LDFLAGS = \
+	#
+
+# Flags included by the user.
+kernel_USER_CPPAFLAGS = \
+	#
+
+# Flags included by the user.
+kernel_USER_CPPFLAGS = \
+	-fno-strict-aliasing \
+	-fwrapv \
+	#
