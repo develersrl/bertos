d57ed7ecf592540f90a714813bd5619cac825306
batt <batt@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-07-02 10:49:47 +0000

    Recode files to unix newlines.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@3987 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/kern/kfile.c
+++ bertos/kern/kfile.c
@@ -1,5 +1,4 @@
 // notest: all
 
 #warning "This file is deprecated, use the new io/kfile.c"
-#error analc
 #include <io/kfile.c>
--- bertos/kern/kfile.h
+++ bertos/kern/kfile.h
@@ -1,3 +1,2 @@
 #warning "This file is deprecated, include the new <io/kfile.h>"
-#error anal
 #include <io/kfile.h>
