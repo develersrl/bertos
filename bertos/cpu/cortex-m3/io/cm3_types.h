f7f68ef253b16438a1676ab594e8f53f51dfe418
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-07-26 15:33:27 +0000

    Move cm3 type to generic types header.
    git-svn-id: https://src.develer.com/svnoss/bertos/branches/i2c@4072 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/cpu/cortex-m3/io/cm3_types.h
+++ bertos/cpu/cortex-m3/io/cm3_types.h
@@ -36,23 +36,5 @@
 #ifndef CM3_TYPES_H
 #define CM3_TYPES_H
 
-/**
- * Macros for hardware access, both direct and via the bit-band region.
- */
-/*\{*/
-#define HWREG(x) (*((reg32_t *)(x)))
-#define HWREGH(x) (*((reg16_t *)(x)))
-#define HWREGB(x) (*((reg8_t *)(x)))
-
-#define HWREGBITW(x, b) \
-        HWREG(((reg32_t)(x) & 0xF0000000) | 0x02000000 |		\
-              (((reg32_t)(x) & 0x000FFFFF) << 5) | ((b) << 2))
-#define HWREGBITH(x, b) \
-        HWREGH(((reg32_t)(x) & 0xF0000000) | 0x02000000 |		\
-               (((reg32_t)(x) & 0x000FFFFF) << 5) | ((b) << 2))
-#define HWREGBITB(x, b) \
-        HWREGB(((reg32_t)(x) & 0xF0000000) | 0x02000000 |		\
-               (((reg32_t)(x) & 0x000FFFFF) << 5) | ((b) << 2))
-/*\}*/
 
 #endif /* CM3_TYPES_H */
