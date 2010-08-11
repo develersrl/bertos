49f91f6a14dbe4cee94d5c16621774b5992d93cf
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-08-11 14:43:42 +0000

    Typo.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@4168 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/cpu/cortex-m3/drv/flash_stm32.c
+++ bertos/cpu/cortex-m3/drv/flash_stm32.c
@@ -146,7 +146,7 @@ static void stm32_flash_clearerror(struct KBlock *blk)
 
 static size_t stm32_flash_readDirect(struct KBlock *blk, block_idx_t idx, void *buf, size_t offset, size_t size)
 {
-	ASSERT(offest == 0);
+	ASSERT(offset == 0);
 	ASSERT(size == blk->blk_size);
 
 	memcpy(buf, (void *)(idx * blk->blk_size), size);
