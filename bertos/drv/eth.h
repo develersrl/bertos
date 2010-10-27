27b6bdaf73e1e61b8ab8760547b68929e7be8837
arighi <arighi@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-10-27 09:43:26 +0000

    AT91: distinct CPU definitions for AT91SAM7X and AT91SAM7S family
    Add distinct definitions for AT91SAM7X and AT91SAM7S families to allow
    the definition of modules that are supported by one architecture and not
    the other (e.g., the ethernet module).
    
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@4472 38d2e660-2303-0410-9eaa-f027e97ec537

--- bertos/drv/eth.h
+++ bertos/drv/eth.h
@@ -36,7 +36,7 @@
  *
  * $WIZ$ module_name = "eth"
  * $WIZ$ module_configuration = "bertos/cfg/cfg_eth.h"
- * $WIZ$ module_supports = "at91"
+ * $WIZ$ module_supports = "at91sam7x"
  */
 
 #ifndef DRV_ETH_H
