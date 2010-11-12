81e3a60fa8f5d52bddc4c1d536afd5425db89349
duplo <duplo@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-11-12 10:31:19 +0000

    Change comment.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@4552 38d2e660-2303-0410-9eaa-f027e97ec537

--- wizard/compatibility.py
+++ wizard/compatibility.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 #
-# This file is part of slimqc.
+# This file is part of BeRTOS.
 #
 # Bertos is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
