15646c02b1d1c7690176e197a66ee1380397fee7
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-11-28 15:53:29 +0000

    Update preset and fix for new name.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@4578 38d2e660-2303-0410-9eaa-f027e97ec537

--- boards/arduino/benchmark/arduino_kernel_footprint/arduino_kernel_footprint_user.mk
+++ boards/arduino/benchmark/arduino_kernel_footprint/arduino_kernel_footprint_user.mk
@@ -4,40 +4,40 @@
 #
 
 # Programmer interface configuration, see http://dev.bertos.org/wiki/ProgrammerInterface for help
-kernel_footprint_PROGRAMMER_TYPE = none
-kernel_footprint_PROGRAMMER_PORT = none
+arduino_kernel_footprint_PROGRAMMER_TYPE = none
+arduino_kernel_footprint_PROGRAMMER_PORT = none
 
 # Files included by the user.
-kernel_footprint_USER_CSRC = \
-	$(kernel_footprint_SRC_PATH)/main.c \
+arduino_kernel_footprint_USER_CSRC = \
+	$(arduino_kernel_footprint_SRC_PATH)/main.c \
 	#
 
 # Files included by the user.
-kernel_footprint_USER_PCSRC = \
+arduino_kernel_footprint_USER_PCSRC = \
 	#
 
 # Files included by the user.
-kernel_footprint_USER_CPPASRC = \
+arduino_kernel_footprint_USER_CPPASRC = \
 	#
 
 # Files included by the user.
-kernel_footprint_USER_CXXSRC = \
+arduino_kernel_footprint_USER_CXXSRC = \
 	#
 
 # Files included by the user.
-kernel_footprint_USER_ASRC = \
+arduino_kernel_footprint_USER_ASRC = \
 	#
 
 # Flags included by the user.
-kernel_footprint_USER_LDFLAGS = \
+arduino_kernel_footprint_USER_LDFLAGS = \
 	#
 
 # Flags included by the user.
-kernel_footprint_USER_CPPAFLAGS = \
+arduino_kernel_footprint_USER_CPPAFLAGS = \
 	#
 
 # Flags included by the user.
-kernel_footprint_USER_CPPFLAGS = \
+arduino_kernel_footprint_USER_CPPFLAGS = \
 	-fno-strict-aliasing \
 	-fwrapv \
 	-Os \
