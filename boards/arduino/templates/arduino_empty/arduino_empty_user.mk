38c0dde41df492a35e4bd674e4f8bb7b1f575f46
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-11-28 20:56:05 +0000

    Update the presets.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@4597 38d2e660-2303-0410-9eaa-f027e97ec537

--- boards/arduino/templates/arduino_empty/arduino_empty_user.mk
+++ boards/arduino/templates/arduino_empty/arduino_empty_user.mk
@@ -4,40 +4,40 @@
 #
 
 # Programmer interface configuration, see http://dev.bertos.org/wiki/ProgrammerInterface for help
-empty_PROGRAMMER_TYPE = none
-empty_PROGRAMMER_PORT = none
+arduino_empty_PROGRAMMER_TYPE = none
+arduino_empty_PROGRAMMER_PORT = none
 
 # Files included by the user.
-empty_USER_CSRC = \
-	$(empty_SRC_PATH)/main.c \
+arduino_empty_USER_CSRC = \
+	$(arduino_empty_SRC_PATH)/main.c \
 	#
 
 # Files included by the user.
-empty_USER_PCSRC = \
+arduino_empty_USER_PCSRC = \
 	#
 
 # Files included by the user.
-empty_USER_CPPASRC = \
+arduino_empty_USER_CPPASRC = \
 	#
 
 # Files included by the user.
-empty_USER_CXXSRC = \
+arduino_empty_USER_CXXSRC = \
 	#
 
 # Files included by the user.
-empty_USER_ASRC = \
+arduino_empty_USER_ASRC = \
 	#
 
 # Flags included by the user.
-empty_USER_LDFLAGS = \
+arduino_empty_USER_LDFLAGS = \
 	#
 
 # Flags included by the user.
-empty_USER_CPPAFLAGS = \
+arduino_empty_USER_CPPAFLAGS = \
 	#
 
 # Flags included by the user.
-empty_USER_CPPFLAGS = \
+arduino_empty_USER_CPPFLAGS = \
 	-fno-strict-aliasing \
 	-fwrapv \
 	#
