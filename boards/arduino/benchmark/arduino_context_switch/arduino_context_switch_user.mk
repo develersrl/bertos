3659d918dd3fb3fe6bd0c69c0f9792bbea709792
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-11-28 15:44:35 +0000

    Update context switch preset and fix to new name.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@4577 38d2e660-2303-0410-9eaa-f027e97ec537

--- boards/arduino/benchmark/arduino_context_switch/arduino_context_switch_user.mk
+++ boards/arduino/benchmark/arduino_context_switch/arduino_context_switch_user.mk
@@ -4,40 +4,40 @@
 #
 
 # Programmer interface configuration, see http://dev.bertos.org/wiki/ProgrammerInterface for help
-context_switch_PROGRAMMER_TYPE = none
-context_switch_PROGRAMMER_PORT = none
+arduino_context_switch_PROGRAMMER_TYPE = none
+arduino_context_switch_PROGRAMMER_PORT = none
 
 # Files included by the user.
-context_switch_USER_CSRC = \
-	$(context_switch_SRC_PATH)/main.c \
+arduino_context_switch_USER_CSRC = \
+	$(arduino_context_switch_SRC_PATH)/main.c \
 	#
 
 # Files included by the user.
-context_switch_USER_PCSRC = \
+arduino_context_switch_USER_PCSRC = \
 	#
 
 # Files included by the user.
-context_switch_USER_CPPASRC = \
+arduino_context_switch_USER_CPPASRC = \
 	#
 
 # Files included by the user.
-context_switch_USER_CXXSRC = \
+arduino_context_switch_USER_CXXSRC = \
 	#
 
 # Files included by the user.
-context_switch_USER_ASRC = \
+arduino_context_switch_USER_ASRC = \
 	#
 
 # Flags included by the user.
-context_switch_USER_LDFLAGS = \
+arduino_context_switch_USER_LDFLAGS = \
 	#
 
 # Flags included by the user.
-context_switch_USER_CPPAFLAGS = \
+arduino_context_switch_USER_CPPAFLAGS = \
 	#
 
 # Flags included by the user.
-context_switch_USER_CPPFLAGS = \
+arduino_context_switch_USER_CPPFLAGS = \
 	-O2 \
 	-fno-strict-aliasing \
 	-fwrapv \
