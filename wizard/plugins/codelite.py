51a6640441a96bcfd40f9029ed8b9db9ca97c215
lottaviano <lottaviano@38d2e660-2303-0410-9eaa-f027e97ec537>
2010-12-22 17:48:12 +0000

    wizard: Fix crash for old python versions
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@4652 38d2e660-2303-0410-9eaa-f027e97ec537

--- wizard/plugins/codelite.py
+++ wizard/plugins/codelite.py
@@ -83,7 +83,7 @@ def findSources(path):
         path += os.sep
     file_dict = {}
     # also follow all symlinks under POSIX OSes
-    if os.name == 'posix':
+    if os.name == 'posix' and sys.version_info >= (2, 6):
         file_list = os.walk(path, followlinks=True)
     else:
         file_list = os.walk(path)
