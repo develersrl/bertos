327e0ae4dd9088fc23bdb2bc230d43d1f1ca4a74
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2011-03-22 14:42:08 +0000

    Update to new name.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@4795 38d2e660-2303-0410-9eaa-f027e97ec537

--- boards/sam3x-ek/examples/sam3x-ek_display/sam3x-ek_display_user.mk
+++ boards/sam3x-ek/examples/sam3x-ek_display/sam3x-ek_display_user.mk
@@ -4,43 +4,43 @@
 #
 
 # Programmer interface configuration, see http://dev.bertos.org/wiki/ProgrammerInterface for help
-display_PROGRAMMER_TYPE = none
-display_PROGRAMMER_PORT = none
+sam3x-ek_display_PROGRAMMER_TYPE = none
+sam3x-ek_display_PROGRAMMER_PORT = none
 
 # Files included by the user.
-display_USER_CSRC = \
-	$(display_SRC_PATH)/main.c \
-	$(display_SRC_PATH)/bitmaps.c \
+sam3x-ek_display_USER_CSRC = \
+	$(sam3x-ek_display_SRC_PATH)/main.c \
+	$(sam3x-ek_display_SRC_PATH)/bitmaps.c \
 	bertos/icons/logo.c \
 	bertos/fonts/gohu.c \
 	#
 
 # Files included by the user.
-display_USER_PCSRC = \
+sam3x-ek_display_USER_PCSRC = \
 	#
 
 # Files included by the user.
-display_USER_CPPASRC = \
+sam3x-ek_display_USER_CPPASRC = \
 	#
 
 # Files included by the user.
-display_USER_CXXSRC = \
+sam3x-ek_display_USER_CXXSRC = \
 	#
 
 # Files included by the user.
-display_USER_ASRC = \
+sam3x-ek_display_USER_ASRC = \
 	#
 
 # Flags included by the user.
-display_USER_LDFLAGS = \
+sam3x-ek_display_USER_LDFLAGS = \
 	#
 
 # Flags included by the user.
-display_USER_CPPAFLAGS = \
+sam3x-ek_display_USER_CPPAFLAGS = \
 	#
 
 # Flags included by the user.
-display_USER_CPPFLAGS = \
+sam3x-ek_display_USER_CPPFLAGS = \
 	-Os \
 	-fno-strict-aliasing \
 	-fwrapv \
