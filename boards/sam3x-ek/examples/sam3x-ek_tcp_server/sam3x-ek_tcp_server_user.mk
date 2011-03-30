2f4f8a91630ed4019ef0cb166cd3c93270b7a28e
aleph <aleph@38d2e660-2303-0410-9eaa-f027e97ec537>
2011-03-30 14:54:01 +0000

    sam3x-ek: add TCP/IP example.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@4827 38d2e660-2303-0410-9eaa-f027e97ec537

---
+++ boards/sam3x-ek/examples/sam3x-ek_tcp_server/sam3x-ek_tcp_server_user.mk
@@ -0,0 +1,55 @@
+#
+# User makefile.
+# Edit this file to change compiler options and related stuff.
+#
+
+# Programmer interface configuration, see http://dev.bertos.org/wiki/ProgrammerInterface for help
+sam3x-ek_tcp_server_PROGRAMMER_TYPE = none
+sam3x-ek_tcp_server_PROGRAMMER_PORT = none
+
+# Files included by the user.
+sam3x-ek_tcp_server_USER_CSRC = \
+	$(sam3x-ek_tcp_server_SRC_PATH)/main.c \
+	#
+
+# Files included by the user.
+sam3x-ek_tcp_server_USER_PCSRC = \
+	#
+
+# Files included by the user.
+sam3x-ek_tcp_server_USER_CPPASRC = \
+	#
+
+# Files included by the user.
+sam3x-ek_tcp_server_USER_CXXSRC = \
+	#
+
+# Files included by the user.
+sam3x-ek_tcp_server_USER_ASRC = \
+	#
+
+# Flags included by the user.
+sam3x-ek_tcp_server_USER_LDFLAGS = \
+	#
+
+# Flags included by the user.
+sam3x-ek_tcp_server_USER_CPPAFLAGS = \
+	#
+
+# Flags included by the user.
+sam3x-ek_tcp_server_USER_CPPFLAGS = \
+	-Os \
+	-fno-strict-aliasing \
+	-fwrapv \
+	#
+
+# Silence very annoying lwip warnings
+lwip_CFLAGS = \
+	-Wno-cast-align \
+	-Wno-cast-qual \
+	-Wno-sign-compare \
+	-Wno-format \
+	-Wno-unused-value \
+	-Wno-conversion \
+	-Wno-missing-noreturn \
+	#
