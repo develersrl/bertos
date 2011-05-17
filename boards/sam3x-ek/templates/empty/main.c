3a818a215a6471c6e6f9a474f4a8d17be5cece4a
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2011-05-17 10:10:47 +0000

    Add copyright and fix includes.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@4905 38d2e660-2303-0410-9eaa-f027e97ec537

--- boards/sam3x-ek/templates/empty/main.c
+++ boards/sam3x-ek/templates/empty/main.c
@@ -1,7 +1,50 @@
-#include <cpu/irq.h>
+/**
+ * \file
+ * <!--
+ * This file is part of BeRTOS.
+ *
+ * Bertos is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, you may use this file as part of a free software
+ * library without restriction.  Specifically, if other files instantiate
+ * templates or use macros or inline functions from this file, or you compile
+ * this file and link it with other files to produce an executable, this
+ * file does not by itself cause the resulting executable to be covered by
+ * the GNU General Public License.  This exception does not however
+ * invalidate any other reasons why the executable file might be covered by
+ * the GNU General Public License.
+ *
+ * Copyright 2011 Develer S.r.l. (http://www.develer.com/)
+ *
+ * -->
+ *
+ * \author Stefano Federico <aleph@develer.com>
+ *
+ * \brief Empty project.
+ *
+ * This is a minimalist project, it just initializes the hardware of the
+ * supported board and proposes an empty main.
+ */
+
+#include "hw/hw_led.h"
+
 #include <cfg/debug.h>
+
+#include <cpu/irq.h>
+
 #include <drv/timer.h>
-#include <hw/hw_led.h>
 #include <drv/lcd_hx8347.h>
 
 static void init(void)
