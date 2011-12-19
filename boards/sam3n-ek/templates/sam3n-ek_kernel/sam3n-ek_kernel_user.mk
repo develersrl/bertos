3a92077cb4ff6601576aa96805ae99f24baf6447
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2011-12-19 17:32:20 +0000

    Fix and update template project.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@5187 38d2e660-2303-0410-9eaa-f027e97ec537

--- boards/sam3n-ek/templates/sam3n-ek_kernel/sam3n-ek_kernel_user.mk
+++ boards/sam3n-ek/templates/sam3n-ek_kernel/sam3n-ek_kernel_user.mk
@@ -4,41 +4,41 @@
 #
 
 # Programmer interface configuration, see http://dev.bertos.org/wiki/ProgrammerInterface for help
-kernel_PROGRAMMER_TYPE = none
-kernel_PROGRAMMER_PORT = none
+sam3n-ek_kernel_PROGRAMMER_TYPE = none
+sam3n-ek_kernel_PROGRAMMER_PORT = none
 
 # Files included by the user.
-kernel_USER_CSRC = \
-	$(kernel_SRC_PATH)/main.c \
-	bertos/cpu/arm/drv/spi_dma_at91.c \
+sam3n-ek_kernel_USER_CSRC = \
+	$(sam3n-ek_kernel_SRC_PATH)/main.c \
+	bertos/cpu/cortex-m3/drv/spi_dma_sam3.c \
 	#
 
 # Files included by the user.
-kernel_USER_PCSRC = \
+sam3n-ek_kernel_USER_PCSRC = \
 	#
 
 # Files included by the user.
-kernel_USER_CPPASRC = \
+sam3n-ek_kernel_USER_CPPASRC = \
 	#
 
 # Files included by the user.
-kernel_USER_CXXSRC = \
+sam3n-ek_kernel_USER_CXXSRC = \
 	#
 
 # Files included by the user.
-kernel_USER_ASRC = \
+sam3n-ek_kernel_USER_ASRC = \
 	#
 
 # Flags included by the user.
-kernel_USER_LDFLAGS = \
+sam3n-ek_kernel_USER_LDFLAGS = \
 	#
 
 # Flags included by the user.
-kernel_USER_CPPAFLAGS = \
+sam3n-ek_kernel_USER_CPPAFLAGS = \
 	#
 
 # Flags included by the user.
-kernel_USER_CPPFLAGS = \
+sam3n-ek_kernel_USER_CPPFLAGS = \
 	-fno-strict-aliasing \
 	-fwrapv \
 	#
