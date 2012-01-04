b0bcaaec638f118bf268bb5bcb5cbd34e7e8e73f
asterix <asterix@38d2e660-2303-0410-9eaa-f027e97ec537>
2012-01-04 10:18:48 +0000

    Update prj.
    git-svn-id: https://src.develer.com/svnoss/bertos/trunk@5198 38d2e660-2303-0410-9eaa-f027e97ec537

--- boards/stm32-p103/templates/stm32-p103_empty/stm32-p103_empty_user.mk
+++ boards/stm32-p103/templates/stm32-p103_empty/stm32-p103_empty_user.mk
@@ -4,40 +4,40 @@
 #
 
 # Programmer interface configuration, see http://dev.bertos.org/wiki/ProgrammerInterface for help
-empty_PROGRAMMER_TYPE = none
-empty_PROGRAMMER_PORT = none
+stm32-p103_empty_PROGRAMMER_TYPE = none
+stm32-p103_empty_PROGRAMMER_PORT = none
 
 # Files included by the user.
-empty_USER_CSRC = \
-	$(empty_SRC_PATH)/main.c \
+stm32-p103_empty_USER_CSRC = \
+	$(stm32-p103_empty_SRC_PATH)/main.c \
 	#
 
 # Files included by the user.
-empty_USER_PCSRC = \
+stm32-p103_empty_USER_PCSRC = \
 	#
 
 # Files included by the user.
-empty_USER_CPPASRC = \
+stm32-p103_empty_USER_CPPASRC = \
 	#
 
 # Files included by the user.
-empty_USER_CXXSRC = \
+stm32-p103_empty_USER_CXXSRC = \
 	#
 
 # Files included by the user.
-empty_USER_ASRC = \
+stm32-p103_empty_USER_ASRC = \
 	#
 
 # Flags included by the user.
-empty_USER_LDFLAGS = \
+stm32-p103_empty_USER_LDFLAGS = \
 	#
 
 # Flags included by the user.
-empty_USER_CPPAFLAGS = \
+stm32-p103_empty_USER_CPPAFLAGS = \
 	#
 
 # Flags included by the user.
-empty_USER_CPPFLAGS = \
+stm32-p103_empty_USER_CPPFLAGS = \
 	-fno-strict-aliasing \
 	-fwrapv \
 	#
