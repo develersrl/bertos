c2c5f82f46658fd3c95f49bbbe30d4dfc676bb6f
Daniele Basile <asterix@develer.com>
2012-02-16 17:20:26 +0100

    Add support for stm32f100c4.

--- bertos/cpu/detect.h
+++ bertos/cpu/detect.h
@@ -214,6 +214,14 @@
 		#define CPU_CM3_STM32F100RB 0
 	#endif
 
+	#if defined (__ARM_STM32F100C4__)
+		#define CPU_CM3_STM32       1
+		#define CPU_CM3_STM32F100C4 1
+		#define CPU_NAME            "STM32F100C4"
+	#else
+		#define CPU_CM3_STM32F100C4 0
+	#endif
+
 	#if defined (__ARM_STM32F101C4__)
 		#define CPU_CM3_STM32       1
 		#define CPU_CM3_STM32F101C4 1
@@ -245,6 +253,7 @@
 	#else
 		#define CPU_CM3_STM32F103RE 0
 	#endif
+
 
 	// AT91SAM3N products serie
 	#if defined (__ARM_SAM3N4__)
@@ -311,7 +320,8 @@
 		#define CPU_CM3_STM32       0
 		#define CPU_CM3_SAM3        0
 	#elif defined (CPU_CM3_STM32)
-		#if CPU_CM3_STM32F100RB + CPU_CM3_STM32F101C4 + CPU_CM3_STM32F103RB + CPU_CM3_STM32F103RE + CPU_CM3_STM32F102C4 + 0 != 1
+		#if CPU_CM3_STM32F100RB + CPU_CM3_STM32F100C4 + CPU_CM3_STM32F101C4 + CPU_CM3_STM32F103RB + CPU_CM3_STM32F103RE + \
+			CPU_CM3_STM32F102C4 + 0 != 1
 			#error STM32 Cortex-M3 CPU configuration error
 		#endif
 		#define CPU_CM3_LM3S        0
@@ -345,6 +355,7 @@
 
 	#define CPU_CM3_STM32 0
 	#define CPU_CM3_STM32F100RB 0
+	#define CPU_CM3_STM32F100C4 0
 	#define CPU_CM3_STM32F103RB 0
 	#define CPU_CM3_STM32F101C4 0
 	#define CPU_CM3_STM32F103RE 0
